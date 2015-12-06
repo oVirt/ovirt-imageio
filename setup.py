b6894ae293ed003310aeea3911d3469c5a0eb448
Amit Aviram <aaviram@redhat.com>
2015-12-06 17:41:17 +0200

    Installation: add setup.py
    Use distutils for easy setup of imaged.
    
    Change-Id: I5360876b83665cd4447cd1ee6d417c6832700102
    Signed-off-by: Amit Aviram <aaviram@redhat.com>

---
+++ setup.py
@@ -0,0 +1,21 @@
+# vdsm-imaged - vdsm image daemon
+# Copyright (C) 2015 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from distutils.core import setup
+
+setup(
+    author="Nir Soffer, Greg Padgett, Amit Aviram",
+    author_email="nsoffer@redhat.com, gpadgett@redhat.com, aaviram@redhat.com",
+    description="vdsm image daemon",
+    license="GNU GPLv2+",
+    name="vdsm-imaged",
+    platforms=["Linux"],
+    packages=["imaged"],
+    url="https://github.com/ovirt/vdsm-imaged",
+    version="0.1",
+)
