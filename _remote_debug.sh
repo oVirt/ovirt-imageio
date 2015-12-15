c9d9d0dbd807ef434cb088fceb477fe3d87ca831
Greg Padgett <gpadgett@redhat.com>
2015-12-15 16:54:48 -0500

    proxy: remove unnecessary files
    Remove files from local debugging that aren't needed in the
    distribution.
    
    Change-Id: Ib25b8fbb8cc06348cbe1c83d5d35b6a8a87e87ad
    Signed-off-by: Greg Padgett <gpadgett@redhat.com>

--- _remote_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ev
-
-IP=192.168.1.52
-
-ssh root@${IP} 'mkdir -p /root/ovirt-image-proxy'
-scp -rp * root@${IP}:/root/ovirt-image-proxy/
--- _run_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-
-#./ovirt-image-proxy --log_config_file=conf/logger.conf --config_file=conf/config.ini --stdout
-
-./ovirt-image-proxy --log_config_file=conf/logger.conf --config_file=_config_overrides.ini --stdout=debug #--pydevd localhost:22200
