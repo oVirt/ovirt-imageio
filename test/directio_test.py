58c28a102104dd075f6afe349352c19f9475eb86
Nir Soffer <nsoffer@redhat.com>
2015-12-19 06:55:00 +0200

    tests: Remove test duplication using parameterization
    Use @pytest.mark.parameterize() to eliminate duplicate test code and
    improve coverage.
    
    Change-Id: I3aee864ea06a8892b1abf0feaebabc5e6bc1a6bb
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/directio_test.py
+++ test/directio_test.py
@@ -12,88 +12,49 @@ from imaged import directio
 from imaged import errors
 
 
-def test_copy_from_image_full_blocks(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
+BLOCK = "a" * directio.BLOCKSIZE
+PARTIAL = "b" * 512
+BYTES = "c" * 42
+
+
+class param(str):
+    """ Prevent pytest from showing the value in the test name """
+    def __str__(self):
+        return self[:10]
+
+
+@pytest.mark.parametrize("data", [
+    param(BLOCK * 2),
+    param(BLOCK + PARTIAL * 2),
+    param(BLOCK + PARTIAL + BYTES),
+    param(PARTIAL * 2),
+    param(PARTIAL + BYTES),
+    param(BYTES),
+])
+def test_copy_from_image(tmpdir, data):
     assert copy_from_image(tmpdir, data, len(data)) == data
 
 
-def test_copy_from_image_full_blocks_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_from_image_full_blocks_and_partial_block(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3
-    assert copy_from_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_from_image_full_blocks_and_partial_block_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_full_block_and_partial_and_some(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3 + "c" * 42
-    assert copy_from_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_full_block_and_partial_and_some_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3 + "c" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_partial_block(tmpdir):
-    data = "a" * 512 * 3
-    assert copy_from_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_partial_block_partial_content(tmpdir):
-    data = "a" * 512 * 3
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_partial_block_and_then_some(tmpdir):
-    data = "a" * 512 * 3 + "b" * 42
-    assert copy_from_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_partial_block_and_then_some_partial_content(tmpdir):
-    data = "a" * 512 * 3 + "b" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_some_bytes(tmpdir):
-    data = "a" * 42
-    assert copy_from_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_some_bytes_partial_content(tmpdir):
-    data = "a" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_from_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_from_aligned_image_partial(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
-    size = len(data) - 500
+@pytest.mark.parametrize(
+    "size", [511, 513, len(BLOCK) + 511, len(BLOCK) + 513])
+def test_copy_from_image_partial(tmpdir, size):
+    data = BLOCK * 2
     assert copy_from_image(tmpdir, data, size) == data[:size]
+
+
+@pytest.mark.parametrize("data", [
+    param(BLOCK * 2),
+    param(BLOCK + PARTIAL * 2),
+    param(BLOCK + PARTIAL + BYTES),
+    param(PARTIAL * 2),
+    param(PARTIAL + BYTES),
+    param(BYTES),
+])
+def test_copy_from_image_partial_content(tmpdir, data):
+    with pytest.raises(errors.PartialContent) as e:
+        copy_from_image(tmpdir, data[:-1], len(data))
+    assert e.value.requested == len(data)
+    assert e.value.available == len(data) - 1
 
 
 def copy_from_image(tmpdir, data, size):
@@ -104,87 +65,38 @@ def copy_from_image(tmpdir, data, size):
     return dst.getvalue()
 
 
-def test_copy_to_image_full_blocks(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
+@pytest.mark.parametrize("data", [
+    param(BLOCK * 2),
+    param(BLOCK + PARTIAL * 2),
+    param(BLOCK + PARTIAL + BYTES),
+    param(PARTIAL * 2),
+    param(PARTIAL + BYTES),
+    param(BYTES),
+])
+def test_copy_to_image(tmpdir, data):
     assert copy_to_image(tmpdir, data, len(data)) == data
 
 
-def test_copy_to_image_full_blocks_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-def test_copy_to_image_full_blocks_and_partial_block(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3
-    assert copy_to_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_full_blocks_and_partial_block_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_full_block_and_partial_and_some(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3 + "c" * 42
-    assert copy_to_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_full_block_and_partial_and_some_partial_content(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2 + "b" * 512 * 3 + "c" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_partial_block(tmpdir):
-    data = "a" * 512 * 3
-    assert copy_to_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_partial_block_partial_content(tmpdir):
-    data = "a" * 512 * 3
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_partial_block_and_then_some(tmpdir):
-    data = "a" * 512 * 3 + "b" * 42
-    assert copy_to_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_partial_block_and_then_some_partial_content(tmpdir):
-    data = "a" * 512 * 3 + "b" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_image_some_bytes(tmpdir):
-    data = "a" * 42
-    assert copy_to_image(tmpdir, data, len(data)) == data
-
-
-def test_copy_to_image_some_bytes_partial_content(tmpdir):
-    data = "a" * 42
-    with pytest.raises(errors.PartialContent) as e:
-        copy_to_image(tmpdir, data[:-1], len(data))
-    assert e.value.requested == len(data)
-    assert e.value.available == len(data) - 1
-
-
-def test_copy_to_aligned_image_partial(tmpdir):
-    data = "a" * directio.BLOCKSIZE * 2
-    size = len(data) - 500
+@pytest.mark.parametrize(
+    "size", [511, 513, len(BLOCK) + 511, len(BLOCK) + 513])
+def test_copy_to_image_partial(tmpdir, size):
+    data = BLOCK * 2
     assert copy_to_image(tmpdir, data, size) == data[:size]
+
+
+@pytest.mark.parametrize("data", [
+    param(BLOCK * 2),
+    param(BLOCK + PARTIAL * 2),
+    param(BLOCK + PARTIAL + BYTES),
+    param(PARTIAL * 2),
+    param(PARTIAL + BYTES),
+    param(BYTES),
+])
+def test_copy_to_image_partial_content(tmpdir, data):
+    with pytest.raises(errors.PartialContent) as e:
+        copy_to_image(tmpdir, data[:-1], len(data))
+    assert e.value.requested == len(data)
+    assert e.value.available == len(data) - 1
 
 
 def copy_to_image(tmpdir, data, size):
