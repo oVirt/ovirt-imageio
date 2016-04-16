410c6cb5ada0baeacf58056630f4ce24dfc11f4f
Nir Soffer <nsoffer@redhat.com>
2016-04-16 23:19:02 +0300

    common: Add setup.py
    Change-Id: Id18760e7d83b20dfdfe2e07841331b3b9ff7cd28
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ common/setup.py
@@ -0,0 +1,21 @@
+# ovirt-imaged-daemon
+# Copyright (C) 2015-2016 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from distutils.core import setup
+
+setup(
+    author="Nir Soffer, Greg Padgett, Amit Aviram",
+    author_email="nsoffer@redhat.com, gpadgett@redhat.com, aaviram@redhat.com",
+    description="oVirt imageio common library",
+    license="GNU GPLv2+",
+    name="ovirt-imageio-common",
+    platforms=["Linux"],
+    packages=["ovirt_image_common"],
+    url="https://gerrit.ovirt.org/ovirt-imageio",
+    version="0.1",
+)
