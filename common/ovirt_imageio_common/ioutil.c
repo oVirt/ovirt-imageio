9cc3839fe08c5ff00b5829e32d2b6a1c4b48a5c2
Nir Soffer <nsoffer@redhat.com>
2017-12-16 02:07:28 +0200

    ioutil: Add fallocate()
    fallocate() can be used for efficient zeroing and trimming of files.
    With modern kernels (>4.9) it also support zeroing and trimming of block
    devices.
    
    Change-Id: Ia09f64e3032c1f3f7c25f23ad65848bde7a0a352
    Bug-Url: https://bugzilla.redhat.com/1615144
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- common/ovirt_imageio_common/ioutil.c
+++ common/ovirt_imageio_common/ioutil.c
@@ -10,6 +10,9 @@
 
 #include <Python.h>
 
+#define GNU_SOURCE
+#include <fcntl.h>
+#include <linux/falloc.h>  /* For FALLOC_FL_* on RHEL, glibc < 2.18 */
 #include <sys/ioctl.h>  /* ioctl */
 #include <linux/fs.h>   /* BLKZEROOUT */
 
@@ -105,12 +108,79 @@ out:
     return PyBool_FromLong(res);
 }
 
+PyDoc_STRVAR(py_fallocate_doc, "\
+fallocate(fd, mode, offset, length)\n\
+Allows the caller to directly manipulate the allocated disk space for\n\
+the file referred to by fd for the byte range starting at offset and\n\
+continuing for length bytes.\n\
+\n\
+Arguments\n\
+  fd        file descriptor to operated on (int)\n\
+  mode      the operation to be performed on the given range (int)\n\
+  offset    start of range (int)\n\
+  length    length of range (int)\n\
+\n\
+Modes\n\
+  FALLOC_FL_KEEP_SIZE       file size will not be changed by the\n\
+                            operation\n\
+  FALLOC_FL_PUNCH_HOLE      deallocates space (i.e., creates a hole) in\n\
+                            the given byte range.\n\
+  FALLOC_FL_COLLAPSE_RANGE  removes a byte range from a file, without\n\
+                            leaving a hole.\n\
+  FALLOC_FL_ZERO_RANGE      zeroes space in the byte range.\n\
+\n\
+Raises\n\
+  OSError if the oprartion failed.\n\
+\n\
+See FALLOCATE(2) for more info.\n\
+");
+
+static PyObject *
+py_fallocate(PyObject *self, PyObject *args)
+{
+    int fd;
+    int mode;
+    off_t offset;
+    off_t length;
+    int err;
+
+    if (!PyArg_ParseTuple(args, "iiLL", &fd, &mode, &offset, &length))
+        return NULL;
+
+    Py_BEGIN_ALLOW_THREADS
+    err = fallocate(fd, mode, offset, length);
+    Py_END_ALLOW_THREADS
+
+    if (err != 0)
+        return PyErr_SetFromErrno(PyExc_OSError);
+
+    Py_RETURN_NONE;
+}
+
 static PyMethodDef module_methods[] = {
     {"blkzeroout", (PyCFunction) blkzeroout, METH_VARARGS | METH_KEYWORDS,
         blkzeroout_doc},
     {"is_zero", (PyCFunction) is_zero, METH_VARARGS, is_zero_doc},
+    {"fallocate", (PyCFunction) py_fallocate, METH_VARARGS, py_fallocate_doc},
     {NULL}  /* Sentinel */
 };
+
+static int module_init(PyObject *m)
+{
+    if (PyModule_AddIntConstant(m, "FALLOC_FL_KEEP_SIZE", FALLOC_FL_KEEP_SIZE))
+        return -1;
+
+    if (PyModule_AddIntConstant(m, "FALLOC_FL_PUNCH_HOLE", FALLOC_FL_PUNCH_HOLE))
+        return -1;
+
+    if (PyModule_AddIntConstant(m, "FALLOC_FL_COLLAPSE_RANGE", FALLOC_FL_COLLAPSE_RANGE))
+        return -1;
+
+    if (PyModule_AddIntConstant(m, "FALLOC_FL_ZERO_RANGE", FALLOC_FL_ZERO_RANGE))
+        return -1;
+
+    return 0;
+}
 
 #define MODULE_NAME "ioutil"
 #define MODULE_DOC "Low level I/O utilities"
@@ -128,7 +198,16 @@ static struct PyModuleDef moduledef = {
 PyMODINIT_FUNC
 PyInit_ioutil(void)
 {
-    return PyModule_Create(&moduledef);
+    PyObject *m;
+
+    m = PyModule_Create(&moduledef);
+    if (m == NULL)
+        return NULL;
+
+    if (module_init(m))
+        return NULL;
+
+    return m;
 }
 
 #else
@@ -136,7 +215,13 @@ PyInit_ioutil(void)
 PyMODINIT_FUNC
 initioutil(void)
 {
-    Py_InitModule3(MODULE_NAME, module_methods, MODULE_DOC);
+    PyObject *m;
+
+    m = Py_InitModule3(MODULE_NAME, module_methods, MODULE_DOC);
+    if (m == NULL)
+        return;
+
+    module_init(m);
 }
 
 #endif
--- common/test/ioutil_test.py
+++ common/test/ioutil_test.py
@@ -8,6 +8,7 @@
 
 from __future__ import absolute_import
 
+import errno
 import os
 import subprocess
 
@@ -226,3 +227,111 @@ def test_is_not_zero_mmap_head(aligned_buffer):
 def test_is_not_zero_mmap(aligned_buffer):
     aligned_buffer[-1:] = b"x"
     assert not ioutil.is_zero(aligned_buffer)
+
+
+# fallocate
+
+fallocate_mode = pytest.mark.parametrize("mode", [
+    # Zero byte range, allocating space - for preallocated images.
+    # Not supported on NFS 4.2.
+    ioutil.FALLOC_FL_ZERO_RANGE,
+    # Zero byte range, daallocating space - for sparse images.
+    # Supported on NFS 4.2.
+    ioutil.FALLOC_FL_PUNCH_HOLE | ioutil.FALLOC_FL_KEEP_SIZE,
+])
+
+
+@fallocate_mode
+def test_fallocate_zero_start(tmpdir, mode):
+    path = str(tmpdir.join("file"))
+    with open(path, "wb") as f:
+        f.write(b"x" * BLOCKSIZE * 3)
+
+    buf = directio.aligned_buffer(BLOCKSIZE * 3)
+    with closing(buf), directio.open(path, "r+") as f:
+        try_fallocate(f.fileno(), mode, 0, BLOCKSIZE)
+
+        n = f.readinto(buf)
+        assert n == BLOCKSIZE * 3
+        assert buf[:BLOCKSIZE] == b"\0" * BLOCKSIZE
+        assert buf[BLOCKSIZE:] == b"x" * BLOCKSIZE * 2
+        assert f.readinto(buf) == 0
+
+
+@fallocate_mode
+def test_fallocate_zero_middle(tmpdir, mode):
+    path = str(tmpdir.join("file"))
+    with open(path, "wb") as f:
+        f.write(b"x" * BLOCKSIZE * 3)
+
+    buf = directio.aligned_buffer(BLOCKSIZE * 3)
+    with closing(buf), directio.open(path, "r+") as f:
+        try_fallocate(f.fileno(), mode, BLOCKSIZE, BLOCKSIZE)
+
+        n = f.readinto(buf)
+        assert n == BLOCKSIZE * 3
+        assert buf[:BLOCKSIZE] == b"x" * BLOCKSIZE
+        assert buf[BLOCKSIZE:-BLOCKSIZE] == b"\0" * BLOCKSIZE
+        assert buf[-BLOCKSIZE:] == b"x" * BLOCKSIZE
+        assert f.readinto(buf) == 0
+
+
+@fallocate_mode
+def test_fallocate_zero_end(tmpdir, mode):
+    path = str(tmpdir.join("file"))
+    with open(path, "wb") as f:
+        f.write(b"x" * BLOCKSIZE * 3)
+
+    buf = directio.aligned_buffer(BLOCKSIZE * 3)
+    with closing(buf), directio.open(path, "r+") as f:
+        try_fallocate(f.fileno(), mode, BLOCKSIZE * 2, BLOCKSIZE)
+
+        n = f.readinto(buf)
+        assert n == BLOCKSIZE * 3
+        assert buf[:-BLOCKSIZE] == b"x" * BLOCKSIZE * 2
+        assert buf[-BLOCKSIZE:] == b"\0" * BLOCKSIZE
+        assert f.readinto(buf) == 0
+
+
+def test_fallocate_zero_after_end(tmpdir):
+    path = str(tmpdir.join("file"))
+    with open(path, "wb") as f:
+        f.write(b"x" * BLOCKSIZE * 3)
+
+    buf = directio.aligned_buffer(BLOCKSIZE * 4)
+    with closing(buf), directio.open(path, "r+") as f:
+        # Will allocate more space that will return zeros when read.
+        mode = ioutil.FALLOC_FL_ZERO_RANGE
+        try_fallocate(f.fileno(), mode, BLOCKSIZE * 3, BLOCKSIZE)
+
+        n = f.readinto(buf)
+        assert n == BLOCKSIZE * 4
+        assert buf[:-BLOCKSIZE] == b"x" * BLOCKSIZE * 3
+        assert buf[-BLOCKSIZE:] == b"\0" * BLOCKSIZE
+        assert f.readinto(buf) == 0
+
+
+def test_fallocate_punch_hole_after_end(tmpdir):
+    path = str(tmpdir.join("file"))
+    with open(path, "wb") as f:
+        f.write(b"x" * BLOCKSIZE * 3)
+
+    buf = directio.aligned_buffer(BLOCKSIZE * 3)
+    with closing(buf), directio.open(path, "r+") as f:
+        # This does not change file contents or size.
+        mode = ioutil.FALLOC_FL_PUNCH_HOLE | ioutil.FALLOC_FL_KEEP_SIZE
+        try_fallocate(f.fileno(), mode, BLOCKSIZE * 3, BLOCKSIZE)
+
+        n = f.readinto(buf)
+        assert n == BLOCKSIZE * 3
+        assert buf[:] == b"x" * BLOCKSIZE * 3
+        assert f.readinto(buf) == 0
+
+
+def try_fallocate(fd, mode, offset, count):
+    try:
+        ioutil.fallocate(fd, mode, offset, count)
+    except EnvironmentError as e:
+        if e.errno != errno.EOPNOTSUPP:
+            raise
+        pytest.skip("fallocate(mode=%r) not supported" % mode)
