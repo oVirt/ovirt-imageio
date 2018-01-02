cf47b9febe031e8df56dda4a4721b187ca5ca2e5
Nir Soffer <nsoffer@redhat.com>
2018-01-02 14:00:05 +0200

    compat: Introduce the compat module
    This module provides helpers for working with different python versions
    in a uniform way.
    
    The first helper is bufview() - a way to create a readonly (python 2) or
    readwrite (python 3) view into a buffer. Typical use case is to write
    part of a buffer to api supporting the buffer interface.
    
    Change-Id: I8ae0110b8ffaee597b8a3732f37da97b6503a881
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ common/ovirt_imageio_common/compat.py
@@ -0,0 +1,20 @@
+# ovirt-imageio
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+"""
+compat - python compatibility helpers
+"""
+
+import six
+
+if six.PY2:
+    def bufview(buf, pos, size):
+        return buffer(buf, pos, size)
+else:
+    def bufview(buf, pos, size):
+        return memoryview(buf)[pos:pos + size]
