25634708f7de1e9973fcdfaa2c80424afef07881
Nir Soffer <nsoffer@redhat.com>
2018-05-13 21:57:38 +0300

    wsgi: Extract wsgi code to new module
    Extract WSGI related code to new module. This will allow using the same
    server and request handler classes for the unix domain variant.
    
    Change-Id: Ib2c834ad313dd239e138824d4b3131d48fee74a1
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- daemon/ovirt_imageio_daemon/server.py
+++ daemon/ovirt_imageio_daemon/server.py
@@ -15,10 +15,6 @@ import signal
 import sys
 import time
 
-from wsgiref import simple_server
-
-from six.moves import socketserver
-
 import systemd.daemon
 import webob
 
@@ -41,6 +37,7 @@ from . import config
 from . import pki
 from . import uhttp
 from . import tickets
+from . import wsgi
 
 CONF_DIR = "/etc/ovirt-imageio-daemon"
 
@@ -129,9 +126,9 @@ class ImagesService(Service):
 
     def __init__(self, config):
         self._config = config
-        self._server = ThreadedWSGIServer(
+        self._server = wsgi.WSGIServer(
             (config.images.host, config.images.port),
-            WSGIRequestHandler)
+            wsgi.WSGIRequestHandler)
         self._secure_server()
         app = web.Application(config, [(r"/images/(.*)", Images)])
         self._server.set_app(app)
@@ -394,89 +391,6 @@ class Tickets(object):
         else:
             tickets.clear()
         return web.response(status=204)
-
-
-class ThreadedWSGIServer(socketserver.ThreadingMixIn,
-                         simple_server.WSGIServer):
-    """
-    Threaded WSGI HTTP server.
-    """
-    daemon_threads = True
-
-
-class WSGIRequestHandler(simple_server.WSGIRequestHandler):
-    """
-    WSGI request handler using HTTP/1.1.
-    """
-
-    protocol_version = "HTTP/1.1"
-
-    def address_string(self):
-        """
-        Override to avoid slow and unneeded name lookup.
-        """
-        return self.client_address[0]
-
-    def handle(self):
-        """
-        Override to use fixed ServerHandler.
-
-        Copied from wsgiref/simple_server.py, using our ServerHandler.
-        """
-        self.raw_requestline = self.rfile.readline(65537)
-        if len(self.raw_requestline) > 65536:
-            self.requestline = ''
-            self.request_version = ''
-            self.command = ''
-            self.send_error(414)
-            return
-
-        if not self.parse_request():  # An error code has been sent, just exit
-            return
-
-        handler = ServerHandler(
-            self.rfile, self.wfile, self.get_stderr(), self.get_environ()
-        )
-        handler.request_handler = self      # backpointer for logging
-        handler.run(self.server.get_app())
-
-    def log_message(self, format, *args):
-        """
-        Override to avoid unwanted logging to stderr.
-        """
-
-
-class ServerHandler(simple_server.ServerHandler):
-
-    # wsgiref handers ignores the http request handler's protocol_version, and
-    # uses its own version. This results in requests returning HTTP/1.0 instead
-    # of HTTP/1.1 - see https://bugzilla.redhat.com/1512317
-    #
-    # Looking at python source we need to define here:
-    #
-    #   http_version = "1.1"
-    #
-    # Bug adding this break some tests.
-    # TODO: investigate this.
-
-    def write(self, data):
-        """
-        Override to allow writing buffer object.
-
-        Copied from wsgiref/handlers.py, removing the check for StringType.
-        """
-        if not self.status:
-            raise AssertionError("write() before start_response()")
-
-        elif not self.headers_sent:
-            # Before the first output, send the stored headers
-            self.bytes_sent = len(data)    # make sure we know content-length
-            self.send_headers()
-        else:
-            self.bytes_sent += len(data)
-
-        self._write(data)
-        self._flush()
 
 
 class UnixWSGIRequestHandler(uhttp.UnixWSGIRequestHandler):
---
+++ daemon/ovirt_imageio_daemon/wsgi.py
@@ -0,0 +1,93 @@
+# ovirt-imageio
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from wsgiref import simple_server
+from six.moves import socketserver
+
+
+class WSGIServer(socketserver.ThreadingMixIn,
+                 simple_server.WSGIServer):
+    """
+    Threaded WSGI HTTP server.
+    """
+    daemon_threads = True
+
+
+class WSGIRequestHandler(simple_server.WSGIRequestHandler):
+    """
+    WSGI request handler using HTTP/1.1.
+    """
+
+    protocol_version = "HTTP/1.1"
+
+    def address_string(self):
+        """
+        Override to avoid slow and unneeded name lookup.
+        """
+        return self.client_address[0]
+
+    def handle(self):
+        """
+        Override to use fixed ServerHandler.
+
+        Copied from wsgiref/simple_server.py, using our ServerHandler.
+        """
+        self.raw_requestline = self.rfile.readline(65537)
+        if len(self.raw_requestline) > 65536:
+            self.requestline = ''
+            self.request_version = ''
+            self.command = ''
+            self.send_error(414)
+            return
+
+        if not self.parse_request():  # An error code has been sent, just exit
+            return
+
+        handler = ServerHandler(
+            self.rfile, self.wfile, self.get_stderr(), self.get_environ()
+        )
+        handler.request_handler = self      # backpointer for logging
+        handler.run(self.server.get_app())
+
+    def log_message(self, format, *args):
+        """
+        Override to avoid unwanted logging to stderr.
+        """
+
+
+class ServerHandler(simple_server.ServerHandler):
+
+    # wsgiref handers ignores the http request handler's protocol_version, and
+    # uses its own version. This results in requests returning HTTP/1.0 instead
+    # of HTTP/1.1 - see https://bugzilla.redhat.com/1512317
+    #
+    # Looking at python source we need to define here:
+    #
+    #   http_version = "1.1"
+    #
+    # Bug adding this break some tests.
+    # TODO: investigate this.
+
+    def write(self, data):
+        """
+        Override to allow writing buffer object.
+
+        Copied from wsgiref/handlers.py, removing the check for StringType.
+        """
+        if not self.status:
+            raise AssertionError("write() before start_response()")
+
+        elif not self.headers_sent:
+            # Before the first output, send the stored headers
+            self.bytes_sent = len(data)    # make sure we know content-length
+            self.send_headers()
+        else:
+            self.bytes_sent += len(data)
+
+        self._write(data)
+        self._flush()
