acf90b86a8fce6649078f5cb049de868cbc7065a
Daniel Erez <derez@redhat.com>
2018-06-27 13:09:22 +0300

    version: bump to 1.4.1
    Change-Id: I664693d5002d85343f9a995090e7d9d55ee3a310
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- common/ovirt_imageio_common/version.py
+++ common/ovirt_imageio_common/version.py
@@ -9,7 +9,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-string = "1.4.0"
+string = "1.4.1"
 
 if __name__ == "__main__":
     print(string)
--- proxy/ovirt_imageio_proxy/version.py
+++ proxy/ovirt_imageio_proxy/version.py
@@ -8,7 +8,7 @@
 
 from __future__ import print_function
 
-string = "1.4.0"
+string = "1.4.1"
 
 if __name__ == "__main__":
     print(string)
