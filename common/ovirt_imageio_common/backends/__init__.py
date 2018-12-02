5ce9af0a687fc5926e23145ab0331f18bd67367b
Nir Soffer <nsoffer@redhat.com>
2018-12-02 03:24:27 +0200

    backends: Add backends.open() helper
    The backends package provides now an open() methods accepting a ticket
    and opening a backend for this ticket.
    
    Currently only file backend is used. In a future version we will use the
    ticket.url.scheme to select the correct backend.
    
    Change-Id: I021298f5ac5c59e290e0270282e297ef72d779c8
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ common/ovirt_imageio_common/backends/__init__.py
@@ -0,0 +1,20 @@
+# ovirt-imageio
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from __future__ import absolute_import
+
+from . import file
+
+
+def open(ticket):
+    """
+    Open a backend for this ticket.
+    """
+    # TODO: use ticket.url.scheme to select the backend.
+    mode = "r+" if "write" in ticket.ops else "r"
+    return file.open(ticket.url.path, mode)
