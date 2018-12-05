07960640c6508bde37155ad5d4d33af05840ac08
Nir Soffer <nsoffer@redhat.com>
2018-12-05 14:44:53 +0200

    tests: Simplify file tests with tmpfile
    Most of the tests can use tmpfile instead of tmpdir, removing some
    boilerplate.
    
    Change-Id: Id78eb210a8885ca08331828295044ae135918bba
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- common/test/file_test.py
+++ common/test/file_test.py
@@ -19,164 +19,153 @@ from ovirt_imageio_common import util
 from ovirt_imageio_common.backends import file
 
 
-def test_open_write_only(tmpdir):
-    path = str(tmpdir.join("path"))
-    with file.open(path, "w") as f, \
+def test_open_write_only(tmpfile):
+    with file.open(tmpfile, "w") as f, \
             closing(util.aligned_buffer(512)) as buf:
         buf.write(b"x" * 512)
         f.write(buf)
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read() == b"x" * 512
 
 
-def test_open_write_only_truncate(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_open_write_only_truncate(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 512)
-    with file.open(path, "w") as f:
+    with file.open(tmpfile, "w") as f:
         pass
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read() == b""
 
 
-def test_open_read_only(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_open_read_only(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 512)
-    with file.open(path, "r") as f, \
+    with file.open(tmpfile, "r") as f, \
             closing(util.aligned_buffer(512)) as buf:
         f.readinto(buf)
         assert buf[:] == b"x" * 512
 
 
-def test_open_read_write(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_open_read_write(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"a" * 512)
-    with file.open(path, "r+") as f, \
+    with file.open(tmpfile, "r+") as f, \
             closing(util.aligned_buffer(512)) as buf:
         f.readinto(buf)
         buf[:] = b"b" * 512
         f.seek(0)
         f.write(buf)
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read() == b"b" * 512
 
 
 @pytest.mark.parametrize("mode", ["r", "r+"])
-def test_open_no_create(tmpdir, mode):
-    path = str(tmpdir.join("path"))
+def test_open_no_create(mode):
     with pytest.raises(OSError) as e:
-        with file.open(path, mode):
+        with file.open("/no/such/path", mode):
             pass
     assert e.value.errno == errno.ENOENT
 
 
-def test_write_unaligned_offset_complete(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_offset_complete(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 1024)
 
     # Write 10 bytes into the second block.
-    with file.open(path, "r+") as f:
+    with file.open(tmpfile, "r+") as f:
         f.seek(600)
         n = f.write(b"y" * 10)
         assert n == 10
         assert f.tell() == 610
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(600) == b"x" * 600
         assert f.read(10) == b"y" * 10
         assert f.read() == b"x" * (1024 - 610)
 
 
-def test_write_unaligned_offset_inside(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_offset_inside(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 1024)
 
     # Write 12 bytes into the first block.
-    with file.open(path, "r+") as f:
+    with file.open(tmpfile, "r+") as f:
         f.seek(500)
         n = f.write(b"y" * 100)
         assert n == 12
         assert f.tell() == 512
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(500) == b"x" * 500
         assert f.read(12) == b"y" * 12
         assert f.read() == b"x" * 512
 
 
-def test_write_unaligned_offset_at_end(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_offset_at_end(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 1024)
 
     # Write 24 bytes into the last block.
-    with file.open(path, "r+") as f:
+    with file.open(tmpfile, "r+") as f:
         f.seek(1000)
         n = f.write(b"y" * 100)
         assert n == 24
         assert f.tell() == 1024
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(1000) == b"x" * 1000
         assert f.read() == b"y" * 24
 
 
-def test_write_unaligned_offset_after_end(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_offset_after_end(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 512)
 
-    with file.open(path, "r+") as f:
+    with file.open(tmpfile, "r+") as f:
         f.seek(600)
         n = f.write(b"y" * 10)
         assert n == 10
         assert f.tell() == 610
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(512) == b"x" * 512
         assert f.read(88) == b"\0" * 88
         assert f.read(10) == b"y" * 10
         assert f.read() == b"\0" * (1024 - 610)
 
 
-def test_write_unaligned_buffer_slow_path(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_buffer_slow_path(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 1024)
 
     # Perform slow read-modify-write in the second block.
-    with file.open(path, "r+") as f:
+    with file.open(tmpfile, "r+") as f:
         f.seek(512)
         n = f.write(b"y" * 10)
         assert n == 10
         assert f.tell() == 522
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(512) == b"x" * 512
         assert f.read(10) == b"y" * 10
         assert f.read() == b"x" * (1024 - 522)
 
 
-def test_write_unaligned_buffer_fast_path(tmpdir):
-    path = str(tmpdir.join("path"))
-    with io.open(path, "wb") as f:
+def test_write_unaligned_buffer_fast_path(tmpfile):
+    with io.open(tmpfile, "wb") as f:
         f.write(b"x" * 4096)
 
     # Perform short fast write of 6 blocks.
     buf = util.aligned_buffer(3073)
     buf.write(b"y" * 3073)
     with closing(buf):
-        with file.open(path, "r+") as f:
+        with file.open(tmpfile, "r+") as f:
             f.seek(512)
             n = f.write(buf)
             assert n == 3072
             assert f.tell() == 3584
 
-    with io.open(path, "rb") as f:
+    with io.open(tmpfile, "rb") as f:
         assert f.read(512) == b"x" * 512
         assert f.read(3072) == b"y" * 3072
         assert f.read() == b"x" * 512
