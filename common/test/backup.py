b0d3aa4b186684bdf0a65a7be86fcd7270b68090
Nir Soffer <nsoffer@redhat.com>
2019-12-12 00:59:19 +0200

    tests: Refactor backup module
    Break full_backup() to small reusable functions so we can build the test
    rig for incremental backup. Based on Eric Blake's incremental backup
    overview part [1] and [2] most of the steps are the same.
    
    [1] https://www.spinics.net/linux/fedora/libvir/msg174457.html
    [2] https://www.spinics.net/linux/fedora/libvir/msg174528.html
    
    Change-Id: I5226001a04e963e44017251d595967885e16b72e
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- common/test/backup.py
+++ common/test/backup.py
@@ -28,92 +28,129 @@ def full_backup(tmpdir, disk, fmt, nbd_sock):
     Start qemu internal nbd server using address nbd_sock, exposing disk for
     full backup, creating temporary files in tmpdir.
     """
-    log.debug("Creating scratch disk")
     scratch_disk = str(tmpdir.join("scratch.qcow2"))
-    subprocess.check_call([
-        "qemu-img",
-        "create",
-        "-f", "qcow2",
-        "-b", disk,
-        scratch_disk
-    ])
-
+    create_scratch_disk(scratch_disk, disk)
     qmp_sock = nbd.UnixAddress(tmpdir.join("qmp.sock"))
 
     with qemu.run(disk, fmt, qmp_sock, start_cpu=False), \
             qmp.Client(qmp_sock) as c:
-        log.debug("Starting nbd server")
-
-        if nbd_sock.transport == "unix":
-            addr = {
-                "type": "unix",
-                "data": {
-                    "path": nbd_sock.path
-                }
-            }
-        elif nbd_sock.transport == "tcp":
-            addr = {
-                "type": "inet",
-                "data": {
-                    "host": nbd_sock.host,
-                    # Qemu wants port as string.
-                    "port": str(nbd_sock.port),
-                }
-            }
-        else:
-            raise RuntimeError("Unsupported transport: {}".format(nbd_sock))
-
-        c.execute("nbd-server-start", {"addr": addr})
-
-        node = qmp.find_node(c, disk)
-        log.debug("Adding backup node for %s", node)
-        c.execute("blockdev-add", {
-            "driver": "qcow2",
-            "node-name": "backup-sda",
-            "file": {
-                "driver": "file",
-                "filename": scratch_disk,
-            },
-            "backing": node["device"],
-        })
-
-        log.debug("Starting backup job")
-        c.execute("transaction", {
-            'actions': [
-                {
-                    'data': {
-                        'device': node["device"],
-                        'job-id': 'backup-sda',
-                        'sync': 'none',
-                        'target': 'backup-sda'
-                    },
-                    'type': 'blockdev-backup',
-                },
-            ]
-        })
-
-        log.debug("Adding node to nbd server")
-        c.execute("nbd-server-add", {
-            "device": "backup-sda",
-            "name": "sda"
-        })
-
+        start_backup(c, nbd_sock, disk, scratch_disk)
         try:
             yield
         finally:
-            # Give qemu time to detect that the NBD client disconnected before
-            # we tear down the nbd server.
-            log.debug("Waiting before tearing down nbd server")
-            time.sleep(0.1)
+            stop_backup(c)
 
-            log.debug("Removing disk sda from nbd server")
-            c.execute("nbd-server-remove", {"name": "sda"})
 
-            log.debug("Stopping nbd server")
-            c.execute("nbd-server-stop")
+def start_backup(c, nbd_sock, disk, scratch_disk):
+    log.debug("Starting backup")
+    start_nbd_server(c, nbd_sock)
+    device = qmp.find_node(c, disk)["device"]
+    add_backup_node(c, "backup-sda", scratch_disk, device)
+    start_backup_job(c, "job0", "backup-sda", device)
+    add_to_nbd_server(c, "backup-sda", "sda")
 
-            log.debug("Cancelling block job")
-            c.execute("block-job-cancel", {"device": "backup-sda"})
 
-            log.debug("Removing backup node")
-            c.execute("blockdev-del", {"node-name": "backup-sda"})
+def stop_backup(c):
+    # Give qemu time to detect that the NBD client disconnected before
+    # we tear down the nbd server.
+    log.debug("Waiting before tearing down nbd server")
+    time.sleep(0.1)
+
+    log.debug("Stopping backup")
+    remove_from_nbd_server(c, "sda")
+    stop_nbd_server(c)
+    cancel_block_job(c, "job0")
+    remove_backup_node(c, "backup-sda")
+
+
+def create_scratch_disk(path, disk):
+    log.debug("Creating scratch disk")
+    subprocess.check_call(
+        ["qemu-img", "create", "-f", "qcow2", "-b", disk, path])
+
+
+def start_nbd_server(c, nbd_sock):
+    log.debug("Starting nbd server listening on %s", nbd_sock)
+
+    if nbd_sock.transport == "unix":
+        addr = {
+            "type": "unix",
+            "data": {
+                "path": nbd_sock.path
+            }
+        }
+    elif nbd_sock.transport == "tcp":
+        addr = {
+            "type": "inet",
+            "data": {
+                "host": nbd_sock.host,
+                # Qemu wants port as string.
+                "port": str(nbd_sock.port),
+            }
+        }
+    else:
+        raise RuntimeError("Unsupported transport: {}".format(nbd_sock))
+
+    c.execute("nbd-server-start", {"addr": addr})
+
+
+def add_backup_node(c, name, scratch_disk, device):
+    log.debug("Adding backup node for %s", device)
+
+    c.execute("blockdev-add", {
+        "driver": "qcow2",
+        "node-name": name,
+        "file": {
+            "driver": "file",
+            "filename": scratch_disk,
+        },
+        "backing": device,
+    })
+
+
+def start_backup_job(c, job_id, target, device):
+    log.debug("Starting backup job")
+
+    actions = [
+        {
+            "type": "blockdev-backup",
+            "data": {
+                "device": device,
+                "job-id": job_id,
+                "sync": "none",
+                "target": target,
+            }
+        }
+    ]
+
+    c.execute("transaction", {
+        "actions": actions
+    })
+
+
+def add_to_nbd_server(c, device, name):
+    log.debug("Adding node to nbd server")
+    c.execute("nbd-server-add", {
+        "device": device,
+        "name": name
+    })
+
+
+def remove_from_nbd_server(c, name):
+    log.debug("Removing export %s from nbd server", name)
+    c.execute("nbd-server-remove", {"name": name})
+
+
+def stop_nbd_server(c):
+    log.debug("Stopping nbd server")
+    c.execute("nbd-server-stop")
+
+
+def cancel_block_job(c, job_id):
+    log.debug("Cancelling block job")
+    c.execute("block-job-cancel", {"device": job_id})
+
+
+def remove_backup_node(c, node_name):
+    log.debug("Removing backup node %s", node_name)
+    c.execute("blockdev-del", {"node-name": node_name})
