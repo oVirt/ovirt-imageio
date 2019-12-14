ce338470fb226b3c81514ef77f686828b17d9099
Nir Soffer <nsoffer@redhat.com>
2019-12-14 21:18:50 +0200

    tests: Add full backup test with real guest
    This is a rather slow test but it does a lot:
    - Create a cirros base image if needed
    - Create an overlay on the base image to ensure test isolation
    - Start a VM from the overlay
    - Create a file in the guest
    - Starts a full backup with a checkpoint
    - Create another file in the guest
    - Copy backup data using qemu-img convert
    - Stop the backup
    - Create an overlay on the backup image
    - Start another VM from the overlay
    - Check that the file created before the backup exist and the file
      created during the backup does not.
    
    This tests only our backup testing infrastructure; we need to add a new
    test using our nbd client or replace qemu-img with the nbd client.
    
    On may laptop, this test takes 2.8 seconds. On Travis it takes 17.6
    seconds since we don't have hardware acceleration. This test always
    timeout on Jenkins so it is marked as xfail there.
    
    Change-Id: Ia62177d52390bcf8fdefd31671987aa5cf5e15d8
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- common/test/backup_test.py
+++ common/test/backup_test.py
@@ -10,14 +10,17 @@ from __future__ import absolute_import
 
 import io
 import logging
+import os
 
 import pytest
 
 from ovirt_imageio_common import nbd
 
 from . import backup
+from . import qemu
 from . import qemu_img
 from . import qemu_nbd
+from . import qmp
 from . import testutil
 
 log = logging.getLogger("test")
@@ -62,3 +65,64 @@ def test_full_backup(tmpdir, fmt, transport):
         for i in range(0, disk_size, disk_part):
             b.seek(i)
             assert d.read(i, 512) == b.read(512)
+
+
+# This can take more than 30 seoconds when running on Travis without hardware
+# acceleration:
+# 41.77s setup    test/backup_test.py::test_full_backup_guest
+# 17.59s call     test/backup_test.py::test_full_backup_guest
+@pytest.mark.timeout(120)
+@pytest.mark.xfail(
+    "OVIRT_CI" in os.environ, reason="Always times out", run=False)
+def test_full_backup_guest(tmpdir, base_image):
+    disk_size = qemu_img.info(base_image)["virtual-size"]
+
+    disk = str(tmpdir.join("disk.qcow2"))
+    qemu_img.create(disk, "qcow2", backing=base_image)
+
+    scratch_disk = str(tmpdir.join("scratch.qcow2"))
+    qemu_img.create(scratch_disk, "qcow2", size=disk_size)
+
+    backup_disk = str(tmpdir.join("backup.qcow2"))
+    qemu_img.create(backup_disk, "qcow2", size=disk_size)
+
+    qmp_sock = nbd.UnixAddress(tmpdir.join("qmp.sock"))
+    nbd_sock = nbd.UnixAddress(tmpdir.join("nbd.sock"))
+
+    log.info("Starting vm")
+
+    with qemu.run(disk, "qcow2", qmp_sock) as guest, \
+            qmp.Client(qmp_sock) as qmp_client:
+
+        log.info("Logging in")
+        guest.login("root", "")
+
+        log.info("Creating file before backup")
+        assert guest.run("touch before-backup; sync") == ""
+
+        log.info("Statring full backup check1")
+        backup.start_backup(
+            qmp_client, nbd_sock, scratch_disk, checkpoint="check1")
+
+        log.info("Creating file during backup")
+        assert guest.run("touch during-backup; sync") == ""
+
+        log.info("Backing up disk to %s", backup_disk)
+        qemu_img.convert(
+            nbd_sock.url("sda"), backup_disk, src_fmt="raw", dst_fmt="qcow2")
+
+        log.info("Stopping backup check1")
+        backup.stop_backup(qmp_client)
+
+    log.info("Verifying backup")
+
+    preview_disk = str(tmpdir.join("preview.qcow2"))
+    qemu_img.create(preview_disk, "qcow2", backing=backup_disk)
+
+    with qemu.run(preview_disk, "qcow2", qmp_sock) as guest:
+
+        log.info("Logging in")
+        guest.login("root", "")
+
+        out = guest.run("ls -1 --color=never")
+        assert out.splitlines() == ["before-backup"]
