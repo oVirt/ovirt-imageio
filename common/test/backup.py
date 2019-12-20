cde24199919b6b3851f56c0abf10d0866752c74d
Nir Soffer <nsoffer@redhat.com>
2019-12-20 03:41:48 +0200

    tests: Extract Backup class
    Passing around all the backup arguments around is very confusing, and it
    is going to be worth when adding incremental backup support. Extract a
    Backup class keeping all the information needed to configure backup, and
    convert the module functions to methods.
    
    Change-Id: I4f06caa20e8f54b863271544fe1b57ecf418eb22
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- common/test/backup.py
+++ common/test/backup.py
@@ -27,153 +27,156 @@ log = logging.getLogger("backup")
 
 
 @contextmanager
-def full_backup(tmpdir, disk, fmt, nbd_sock, checkpoint=None):
+def full_backup(tmpdir, disk, fmt, sock, checkpoint=None):
     """
-    Start qemu internal nbd server using address nbd_sock, exposing disk for
+    Start qemu internal nbd server using address sock, exposing disk for
     full backup, creating temporary files in tmpdir.
     """
     scratch_disk = str(tmpdir.join("scratch.qcow2"))
-    create_scratch_disk(scratch_disk, disk)
     qmp_sock = nbd.UnixAddress(tmpdir.join("qmp.sock"))
+
+    disk_size = qemu_img.info(disk)["virtual-size"]
+    qemu_img.create(scratch_disk, "qcow2", size=disk_size)
 
     with qemu.run(disk, fmt, qmp_sock, start_cpu=False, shutdown_timeout=10), \
             qmp.Client(qmp_sock) as c, \
-            run(c, nbd_sock, scratch_disk, checkpoint=checkpoint):
+            run(c, sock, scratch_disk, checkpoint=checkpoint):
         yield
 
 
 @contextmanager
-def run(c, nbd_sock, scratch_disk, checkpoint=None):
-    start_backup(c, nbd_sock, scratch_disk, checkpoint=checkpoint)
+def run(qmp, sock, scratch_disk, checkpoint=None, incremental=None):
+    b = Backup(qmp, sock, scratch_disk, checkpoint=checkpoint)
+    b.start()
     try:
-        yield
+        yield b
     finally:
-        stop_backup(c)
+        b.stop()
 
 
-def start_backup(c, nbd_sock, scratch_disk, checkpoint=None):
-    log.info("Statring backup checkpoint=%s", checkpoint)
-    start_nbd_server(c, nbd_sock)
-    # Use node name "file0" as a stable reference to our disk. It will not
-    # change when the block graph is modifed during backup.
-    add_backup_node(c, "backup-sda", scratch_disk, "file0")
-    start_backup_job(c, "job0", "backup-sda", "file0", checkpoint=checkpoint)
-    add_to_nbd_server(c, "backup-sda", "sda")
+class Backup(object):
 
+    def __init__(self, qmp, sock, scratch_disk, checkpoint=None):
+        self.qmp = qmp
+        self.sock = sock
+        self.scratch_disk = scratch_disk
+        self.checkpoint = checkpoint
 
-def stop_backup(c):
-    log.info("Stopping backup")
-    # Give qemu time to detect that the NBD client disconnected before
-    # we tear down the nbd server.
-    log.debug("Waiting before tearing down nbd server")
-    time.sleep(0.1)
+        # Hardcoded value, good enough for now.
+        self.export = "sda"
 
-    log.debug("Stopping backup")
-    remove_from_nbd_server(c, "sda")
-    stop_nbd_server(c)
-    cancel_block_job(c, "job0")
-    remove_backup_node(c, "backup-sda")
+        # Use node name "file0" as a stable reference to our disk. It will not
+        # change when the block graph is modifed during backup.
+        self.file = "file0"
 
+        self.job = "job0"
 
-def create_scratch_disk(path, disk):
-    log.debug("Creating scratch disk")
-    qemu_img.create(path, "qcow2", backing=disk)
+        # Libvirt uses something like "backup-libvit-42-format".
+        self.node = "backup-file0"
 
+    def start(self):
+        log.info("Starting backup checkpoint=%s", self.checkpoint)
 
-def start_nbd_server(c, nbd_sock):
-    log.debug("Starting nbd server listening on %s", nbd_sock)
+        self.start_nbd_server()
+        self.add_backup_node()
+        self.run_backup_transaction()
+        self.add_to_nbd_server()
 
-    if nbd_sock.transport == "unix":
-        addr = {
-            "type": "unix",
-            "data": {
-                "path": nbd_sock.path
-            }
-        }
-    elif nbd_sock.transport == "tcp":
-        addr = {
-            "type": "inet",
-            "data": {
-                "host": nbd_sock.host,
-                # Qemu wants port as string.
-                "port": str(nbd_sock.port),
-            }
-        }
-    else:
-        raise RuntimeError("Unsupported transport: {}".format(nbd_sock))
+    def stop(self):
+        log.info("Stopping backup checkpoint=%s", self.checkpoint)
 
-    c.execute("nbd-server-start", {"addr": addr})
+        # Give qemu time to detect that the NBD client disconnected before
+        # we tear down the nbd server.
+        log.debug("Waiting before tearing down nbd server")
+        time.sleep(0.1)
 
+        self.remove_from_nbd_server()
+        self.stop_nbd_server()
+        self.cancel_block_job()
+        self.remove_backup_node()
 
-def add_backup_node(c, name, scratch_disk, node_name):
-    log.debug("Adding backup node for %s", node_name)
+    def start_nbd_server(self):
+        log.debug("Starting nbd server listening on %s", self.sock)
 
-    c.execute("blockdev-add", {
-        "driver": "qcow2",
-        "node-name": name,
-        "file": {
-            "driver": "file",
-            "filename": scratch_disk,
-        },
-        "backing": node_name,
-    })
+        if self.sock.transport == "unix":
+            addr = {"type": "unix",
+                    "data": {"path": self.sock.path}}
+        elif self.sock.transport == "tcp":
+            addr = {"type": "inet",
+                    "data": {"host": self.sock.host,
+                             "port": str(self.sock.port)}}
+        else:
+            raise RuntimeError("Unsupported transport: {}".format(self.sock))
 
+        self.qmp.execute("nbd-server-start", {"addr": addr})
 
-def start_backup_job(c, job_id, target, node_name, checkpoint=None):
-    log.debug("Starting backup job")
-    actions = []
+    def add_backup_node(self):
+        log.debug("Adding backup node %s for %s", self.node, self.file)
 
-    if checkpoint:
+        self.qmp.execute("blockdev-add", {
+            "driver": "qcow2",
+            "node-name": self.node,
+            "file": {
+                "driver": "file",
+                "filename": self.scratch_disk,
+            },
+            "backing": self.file,
+        })
+
+    def run_backup_transaction(self):
+        log.debug("Running backup transaction")
+        actions = []
+
+        if self.checkpoint:
+            actions.append({
+                "type": "block-dirty-bitmap-add",
+                "data": {
+                    "name": self.checkpoint,
+                    "node": self.file,
+                    "persistent": True,
+                }
+            })
+
         actions.append({
-            "type": "block-dirty-bitmap-add",
+            "type": "blockdev-backup",
             "data": {
-                "name": checkpoint,
-                "node": node_name,
-                "persistent": True,
+                "device": self.file,
+                "job-id": self.job,
+                "sync": "none",
+                "target": self.node,
             }
         })
 
-    actions.append({
-        "type": "blockdev-backup",
-        "data": {
-            "device": node_name,
-            "job-id": job_id,
-            "sync": "none",
-            "target": target,
-        }
-    })
+        self.qmp.execute("transaction", {"actions": actions})
 
-    c.execute("transaction", {
-        "actions": actions
-    })
+    def add_to_nbd_server(self):
+        log.debug("Adding node %s to nbd server", self.node)
+        arguments = {"name": self.export, "device": self.node}
+        self.qmp.execute("nbd-server-add", arguments)
 
+    def remove_from_nbd_server(self):
+        log.debug("Removing export %s from nbd server", self.export)
+        self.qmp.execute("nbd-server-remove", {"name": self.export})
 
-def add_to_nbd_server(c, device, name):
-    log.debug("Adding node to nbd server")
-    c.execute("nbd-server-add", {
-        "device": device,
-        "name": name
-    })
+    def stop_nbd_server(self):
+        log.debug("Stopping nbd server")
+        self.qmp.execute("nbd-server-stop")
 
+    def cancel_block_job(self):
+        log.debug("Cancelling block job %s", self.job)
+        self.qmp.execute("block-job-cancel", {"device": self.job})
 
-def remove_from_nbd_server(c, name):
-    log.debug("Removing export %s from nbd server", name)
-    c.execute("nbd-server-remove", {"name": name})
+    def remove_backup_node(self):
+        log.debug("Removing backup node %s", self.node)
+        self.qmp.execute("blockdev-del", {"node-name": self.node})
 
-
-def stop_nbd_server(c):
-    log.debug("Stopping nbd server")
-    c.execute("nbd-server-stop")
-
-
-def cancel_block_job(c, job_id):
-    log.debug("Cancelling block job")
-    c.execute("block-job-cancel", {"device": job_id})
-
-
-def remove_backup_node(c, node_name):
-    log.debug("Removing backup node %s", node_name)
-    c.execute("blockdev-del", {"node-name": node_name})
+    def remove_dirty_bitmap(self):
+        log.debug("Removing dirty bitmap %s from node %s",
+                  self.bitmap, self.file)
+        self.qmp.execute("block-dirty-bitmap-remove", {
+            "node": self.file,
+            "name": self.bitmap,
+        })
 
 
 # Backup data helpers.
