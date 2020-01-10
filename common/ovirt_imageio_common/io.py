eb7d3894115913dd480aa67292ff54e5aa32bb60
Nir Soffer <nsoffer@redhat.com>
2020-01-11 00:13:33 +0200

    common: Introduce the io module
    The io module implements io operations for backends. The first operation
    is io.copy(), copying source backup to destination backup.
    
    The copy uses source backend extents to copy only the data extents from
    source backend to destination backend.
    
    By default, io.copy() zero unallocated areas on the destination. When
    copying to new image on file based storage this is not needed; calling
    with zero=False will skip zeroing.
    
    If the destination implements read_from(), it is used to copy data
    extents from source. Otherwise if the source implements write_to(), it
    is used to write data to the destination. Otherwise fallback to generic
    copy that works with backends implementing only readinto() and write().
    
    Finally, large extents are splitted to smaller ones for more frequent
    progress updates when dealing with very large zero or data extents that
    may take lot of time to process.
    
    Change-Id: I0ec38cf5ee330c6cfcd907ddab390d7b8acc8ef7
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ common/ovirt_imageio_common/io.py
@@ -0,0 +1,96 @@
+# ovirt-imageio
+# Copyright (C) 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+"""
+io - I/O operations on backends.
+"""
+
+from __future__ import absolute_import
+
+import logging
+
+# Limit maximum zero and copy size to ensure frequent progress updates when
+# handling large extents.
+MAX_ZERO_SIZE = 1024**3
+MAX_COPY_SIZE = 128 * 1024**2
+
+# NBD hard limit.
+MAX_BUFFER_SIZE = 32 * 1024**2
+
+# TODO: Needs testing.
+BUFFER_SIZE = 4 * 1024**2
+
+log = logging.getLogger("io")
+
+
+def copy(src, dst, buffer_size=BUFFER_SIZE, zero=True, progress=None):
+    buffer_size = min(buffer_size, MAX_BUFFER_SIZE)
+    buf = bytearray(buffer_size)
+
+    for ext in src.extents():
+        if not ext.zero:
+            _copy_extent(src, dst, ext, buf, progress)
+        elif zero:
+            _zero_extent(dst, ext, progress)
+        elif progress:
+            progress.update(ext.length)
+
+    dst.flush()
+
+
+def _copy_extent(src, dst, ext, buf, progress):
+    for start, length in _split(ext.start, ext.length, MAX_COPY_SIZE):
+        src.seek(start)
+        dst.seek(start)
+
+        if hasattr(dst, "read_from"):
+            dst.read_from(src, length, buf)
+        elif hasattr(src, "write_to"):
+            src.write_to(dst, length, buf)
+        else:
+            _generic_copy(src, dst, start, length, buf)
+
+        if progress:
+            progress.update(length)
+
+
+def _zero_extent(dst, ext, progress):
+    # TODO: Assumes complete zero(); works with the nbd and http backends but
+    # not with the file backend.
+    for start, length in _split(ext.start, ext.length, MAX_ZERO_SIZE):
+        dst.seek(start)
+        dst.zero(length)
+        if progress:
+            progress.update(length)
+
+
+def _split(start, length, max_length):
+    """
+    Split big range to smaller ones.
+    """
+    while length > max_length:
+        yield start, max_length
+        length -= max_length
+        start += max_length
+
+    yield start, length
+
+
+def _generic_copy(src, dst, start, length, buf):
+    # TODO: Assumes complete readinto() and write(); works with the nbd and
+    # http backends but not with the file backend.
+    step = len(buf)
+    todo = length
+
+    while todo > step:
+        src.readinto(buf)
+        dst.write(buf)
+        todo -= step
+
+    with memoryview(buf)[:todo] as last:
+        src.readinto(last)
+        dst.write(last)
---
+++ common/test/io_test.py
@@ -0,0 +1,194 @@
+# ovirt-imageio
+# Copyright (C) 2020 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from __future__ import absolute_import
+
+import pytest
+
+from six.moves.urllib_parse import urlparse
+
+from ovirt_imageio_common import qemu_img
+from ovirt_imageio_common import qemu_nbd
+from ovirt_imageio_common import io
+from ovirt_imageio_common.backends import nbd, memory, image
+from ovirt_imageio_common.nbd import UnixAddress
+
+from . marks import requires_python3
+
+pytestmark = requires_python3
+
+ZERO_PARAMS = [
+    pytest.param(True, id="zero"),
+    pytest.param(False, id="nozero"),
+]
+
+
+@pytest.mark.parametrize("src_fmt,dst_fmt", [
+    ("raw", "raw"),
+    ("qcow2", "qcow2"),
+    ("raw", "qcow2"),
+    ("qcow2", "raw"),
+])
+@pytest.mark.parametrize("zero", ZERO_PARAMS)
+def test_copy_nbd_to_nbd(tmpdir, src_fmt, dst_fmt, zero):
+    # Make sure we have zero extents larger than MAX_ZERO_SIZE (1 GiB). It
+    # would be nice to have also data extents larger than MAX_COPY_SIZE (128
+    # MiB), but this is too slow for automated tests.
+    size = 2 * io.MAX_ZERO_SIZE
+
+    # Default cluser size with qcow2 format.
+    cluster_size = 64 * 1024
+
+    src = str(tmpdir.join("src." + src_fmt))
+    qemu_img.create(src, src_fmt, size=size)
+
+    with qemu_nbd.open(src, src_fmt) as c:
+        # Create first data extent.
+        c.write(0, b"data extent 1\n")
+
+        # Between the data extents we have a zero extent bigger than
+        # io.MAX_ZERO_SIZE.
+
+        # Create data extent larger than io.BUFFER_SIZE.
+        data = b"data extent 2\n" + b"x" * io.BUFFER_SIZE
+        c.write(io.MAX_ZERO_SIZE + 2 * cluster_size, data)
+
+        # Between the data extents we have a zero extent smaller than
+        # io.MAX_ZERO_SIZE.
+
+        # Create last data extent at the end of the image.
+        c.write(size - 4096, b"data extent 3\n")
+
+        c.flush()
+
+    src_sock = UnixAddress(tmpdir.join("src.sock"))
+    src_url = urlparse(src_sock.url())
+
+    dst = str(tmpdir.join("dst." + dst_fmt))
+    qemu_img.create(dst, dst_fmt, size=size)
+    dst_sock = UnixAddress(tmpdir.join("dst.sock"))
+    dst_url = urlparse(dst_sock.url())
+
+    with qemu_nbd.run(src, src_fmt, src_sock, read_only=True), \
+            qemu_nbd.run(dst, dst_fmt, dst_sock), \
+            nbd.open(src_url, "r") as src_backend, \
+            nbd.open(dst_url, "r+") as dst_backend:
+
+        # Because we copy to new image, we can alays use zero=False, but we
+        # test both to verify that the result is the same.
+        io.copy(src_backend, dst_backend, zero=zero)
+
+    qemu_img.compare(src, dst)
+
+
+@pytest.mark.parametrize("zero", ZERO_PARAMS)
+def test_copy_generic(zero):
+    size = 1024
+    chunk_size = size // 2
+
+    def fake_extents(context="zero"):
+        return [
+            image.ZeroExtent(0 * chunk_size, chunk_size, False),
+            image.ZeroExtent(1 * chunk_size, chunk_size, True),
+        ]
+
+    src = memory.Backend("r", b"x" * chunk_size + b"\0" * chunk_size)
+    src.extents = fake_extents
+
+    dst = memory.Backend("r+", (b"y" if zero else b"\0") * size)
+
+    io.copy(src, dst, buffer_size=128, zero=zero)
+
+    assert dst.size() == src.size()
+    assert dst.data() == src.data()
+
+
+@pytest.mark.parametrize("zero", ZERO_PARAMS)
+def test_copy_read_from(zero):
+    size = 1024
+    chunk_size = size // 2
+
+    def fake_extents(context="zero"):
+        return [
+            image.ZeroExtent(0 * chunk_size, chunk_size, False),
+            image.ZeroExtent(1 * chunk_size, chunk_size, True),
+        ]
+
+    src = memory.Backend("r", b"x" * chunk_size + b"\0" * chunk_size)
+    src.extents = fake_extents
+
+    dst = memory.ReaderFrom("r+", (b"y" if zero else b"\0") * size)
+
+    io.copy(src, dst, buffer_size=128)
+
+    assert dst.size() == src.size()
+    assert dst.data() == src.data()
+
+
+@pytest.mark.parametrize("zero", ZERO_PARAMS)
+def test_copy_write_to(zero):
+    size = 1024
+    chunk_size = size // 2
+
+    def fake_extents(context="zero"):
+        return [
+            image.ZeroExtent(0 * chunk_size, chunk_size, False),
+            image.ZeroExtent(1 * chunk_size, chunk_size, True),
+        ]
+
+    src = memory.WriterTo("r", b"x" * chunk_size + b"\0" * chunk_size)
+    src.extents = fake_extents
+
+    dst = memory.Backend("r+", (b"y" if zero else b"\0") * size)
+
+    io.copy(src, dst, buffer_size=128, zero=zero)
+
+    assert dst.size() == src.size()
+    assert dst.data() == src.data()
+
+
+class FakeProgress:
+
+    def __init__(self):
+        self.updates = []
+
+    def update(self, n):
+        self.updates.append(n)
+
+
+@pytest.mark.parametrize("zero", ZERO_PARAMS)
+def test_copy_progress(zero):
+    size = 1024
+    chunk_size = size // 4
+
+    def fake_extents(context="zero"):
+        return [
+            image.ZeroExtent(0 * chunk_size, chunk_size, False),
+            image.ZeroExtent(1 * chunk_size, chunk_size, True),
+            image.ZeroExtent(2 * chunk_size, chunk_size, False),
+            image.ZeroExtent(3 * chunk_size, chunk_size, True),
+        ]
+
+    src = memory.Backend("r", (
+        b"x" * chunk_size +
+        b"\0" * chunk_size +
+        b"x" * chunk_size +
+        b"\0" * chunk_size
+    ))
+    src.extents = fake_extents
+
+    dst = memory.Backend("r+", b"\0" * size)
+
+    p = FakeProgress()
+    io.copy(src, dst, zero=zero, progress=p)
+
+    # Report at least every extent.
+    assert len(p.updates) >= 4
+
+    # Report entire image size.
+    assert sum(p.updates) == size
