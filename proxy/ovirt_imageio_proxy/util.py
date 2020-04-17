caf881e8af4807d5996786b9c41ecbf351fe0543
Nir Soffer <nsoffer@redhat.com>
2020-04-18 01:15:02 +0300

    proxy: Remove old proxy
    The proxy was replaced by the daemon in version 2.0.4. The engine setup
    plugin is part of engine now.
    
    Remove the proxy and setup code and leftovers from automation files.
    More work is needed to update the docs.
    
    Change-Id: I3fc7969801a5983c349b6b4f6fd8896b34d979a8
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- proxy/ovirt_imageio_proxy/util.py
+++
@@ -1,8 +0,0 @@
-def to_bool(string):
-    first = str(string).lower()[:1]
-    if first in ('t', 'y', '1'):
-        return True
-    elif first in ('f', 'n', '0'):
-        return False
-    else:
-        raise ValueError("Invalid value for boolean: {0}".format(string))
