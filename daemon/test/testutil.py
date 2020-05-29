5cfba46d5ec26e5e4d0fd1bd7c4b67e3b5142602
Vojtech Juranek <vjuranek@redhat.com>
2020-05-29 10:15:37 +0200

    travis: test IPv6
    Test if we run tests on a machines which has IPv6 supported.
    This should help with debugging tests. Some of the tests uses IPv6 and
    this way we can easily see if IPv6 is configured.
    
    Change-Id: Id16d9d28e98545b2d48ddc94e770d6e9277afb0a
    Signed-off-by: Vojtech Juranek <vjuranek@redhat.com>

--- daemon/test/testutil.py
+++ daemon/test/testutil.py
@@ -8,6 +8,7 @@
 
 from __future__ import absolute_import
 
+import errno
 import logging
 import socket
 from uuid import uuid4
@@ -66,3 +67,15 @@ def create_tempfile(tmpdir, name, data=b'', size=None):
         if data:
             f.write(data)
     return file
+
+
+def ipv6_enabled(dev='default'):
+    # Based on
+    # https://github.com/oVirt/vdsm/blob/v4.40.19/lib/vdsm/network/sysctl.py#L59
+    try:
+        with open('/proc/sys/net/ipv6/conf/%s/disable_ipv6' % dev) as f:
+            return bool(int(f.read()))
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        return False
