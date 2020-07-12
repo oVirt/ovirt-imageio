fdbb994b147d37c03337c082d49d451466a6bc3b
Nir Soffer <nsoffer@redhat.com>
2020-07-12 14:02:22 +0300

    travis: Create loop devices before build
    Looks like we don't have enough loop devices in the container. We had
    the same issue in oVirt CI, which was solved by creating more devices
    nodes in the chroot.
    
    Change-Id: I085104b9e84aba55d0cb71b667947eb9b531b90b
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- travis/test.sh
+++ travis/test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+create_loop_devices() {
+    local last=$(($1-1))
+    local min
+    for min in `seq 0 $last`; do
+        local name=/dev/loop$min
+        if [ ! -e "$name" ]; then
+            echo "Creating loop device $name"
+            mknod --mode 0666 $name b 7 $min
+        fi
+    done
+}
+
+create_loop_devices 16
+
 make
 make storage
 make check
