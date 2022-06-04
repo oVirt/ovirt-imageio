bc5c80c10511e1d4da42a47c63e3d83bd1fa737e
Nir Soffer <nsoffer@redhat.com>
2022-06-04 21:00:50 +0300

    github: Workaround for CVE-2022-24765 fix
    When building on Fedora 36 we get:
    
        fatal: unsafe repository ('/path' is owned by someone else)
    
    when running the tests and creating rpms. The warning is harmless for
    the tests, but it it breaks the rpm version, which make them useless for
    testing builds from github.
    
    Fix both cases by configuring git to trust the current repository.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- ci/rpm.sh
+++ ci/rpm.sh
@@ -3,5 +3,12 @@
 EXPORT_DIR="${EXPORT_DIR:-exported-artifacts}"
 
 mkdir -p "${EXPORT_DIR}"
+
+# Workaround for CVE-2022-24765 fix:
+#   fatal: unsafe repository ('/__w/ovirt-imageio/ovirt-imageio' is owned by
+#   someone else)
+# Required for correct rpm version when not building from tag.
+git config --global --add safe.directory "$(pwd)"
+
 make rpm OUTDIR="${EXPORT_DIR}"
 createrepo_c "${EXPORT_DIR}"
