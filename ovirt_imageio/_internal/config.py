oVirt/ovirt-imageio

commit d5e9c757e0a44fdd88d9e54b77283e718407b9cc

http: Configurable inactivity timeout
We used 60 seconds timeout for disconnecting inactive clients. This is
too long to protect from bad clients leaving open connections, and too
short for applications that need long timeout[1].

Change the default timeout to 15 seconds, configurable via
daemon:auth_timeout. This timeout is used for new unauthorized
connections. If a connection does not authorize within this timeout, it
is disconnected.

When a connection is authorized during the first request, the connection
timeout is increased to ticket.inactivity_timeout. The default value is
60 seconds, configurable via daemon:inactivity_timeout.

Application with special needs can request a larger timeout when
creating an image transfer. Engine need to include the transfer
inactivity timeout in the ticket.

[1] https://bugzilla.redhat.com/2032324

Fixes #14.

Signed-off-by: Nir Soffer <nsoffer@redhat.com>

data/README
@@ -47,6 +47,17 @@
 # The default value:
 #   max_connections = 8
 
+# Number of seconds before unauthorized connections are
+# disconnected.
+# The default value:
+#   auth_timeout = 15
+
+# Number of seconds before inactive connections are disconnected.
+# Clients with special needs can request a larger timeout when
+# creating an image transfer.
+# The default value:
+#   inactivity_timeout = 60
+
 [tls]
 # Enable TLS. Note that without TLS transfer tickets and image data are
 # transferred in clear text. If TLS is enabled, paths to related files

ovirt_imageio/_internal/config.py
@@ -20,6 +20,15 @@ class daemon:
     # decrease throughput.
     max_connections = 8
 
+    # Number of seconds before unauthorized connections are
+    # disconnected.
+    auth_timeout = 15
+
+    # Number of seconds before inactive connections are disconnected.
+    # Clients with special needs can request a larger timeout when
+    # creating an image transfer.
+    inactivity_timeout = 60
+
     # Daemon run directory. Runtime stuff like socket or profile information
     # will be stored in this directory.
     # This is configurable only for development purposes and is not expected to

ovirt_imageio/_internal/auth.py
@@ -22,7 +22,7 @@ log = logging.getLogger("auth")
 
 class Ticket:
 
-    def __init__(self, ticket_dict):
+    def __init__(self, ticket_dict, cfg):
         if not isinstance(ticket_dict, dict):
             raise errors.InvalidTicket(
                 "Invalid ticket: %r, expecting a dict" % ticket_dict)
@@ -32,6 +32,10 @@ class Ticket:
         self._ops = _required(ticket_dict, "ops", list)
 
         self._timeout = _required(ticket_dict, "timeout", int)
+        self._inactivity_timeout = _optional(
+            ticket_dict, "inactivity_timeout", int,
+            default=cfg.daemon.inactivity_timeout)
+
         now = int(util.monotonic_time())
         self._expires = now + self._timeout
         self._access_time = now
@@ -123,6 +127,14 @@ class Ticket:
         if self.active():
             return 0
         return int(util.monotonic_time()) - self._access_time
+
+    @property
+    def inactivity_timeout(self):
+        """
+        Return the number of seconds to wait before disconnecting inactive
+        client.
+        """
+        return self._inactivity_timeout
 
     @property
     def canceled(self):
@@ -249,6 +261,7 @@ class Ticket:
             "connections": len(self._connections),
             "expires": self._expires,
             "idle_time": self.idle_time,
+            "inactivity_timeout": self._inactivity_timeout,
             "ops": list(self._ops),
             "size": self._size,
             "sparse": self._sparse,
@@ -336,6 +349,7 @@ class Ticket:
                 "canceled={self._canceled} "
                 "connections={connections} "
                 "expires={self.expires!r} "
+                "inactivity_timeout={self.inactivity_timeout} "
                 "filename={self.filename!r} "
                 "idle_time={self.idle_time} "
                 "ops={self.ops!r} "
@@ -388,7 +402,7 @@ class Authorizer:
 
         Raises errors.InvalidTicket if ticket dict is invalid.
         """
-        ticket = Ticket(ticket_dict)
+        ticket = Ticket(ticket_dict, self._config)
         self._tickets[ticket.uuid] = ticket
 
     def remove(self, ticket_id):

ovirt_imageio/_internal/backends/__init__.py
@@ -98,6 +98,9 @@ def get(req, ticket, config):
             ctx.close()
             raise
 
+        # Authorized connections get a longer timeout from the ticket.
+        req.set_connection_timeout(ticket.inactivity_timeout)
+
         # Register a closer removing the context when the connection is closed.
         req.context[ticket.uuid] = Closer(
             partial(ticket.remove_context, req.connection_id))

ovirt_imageio/_internal/http.py
@@ -169,10 +169,11 @@ class Connection(http.server.BaseHTTPRequestHandler):
     # to support long URIs, so we use small value.
     max_request_line = 4096
 
-    # Number of second to wait for recv() or send(). When the timeout expires
-    # we close the connection. This is important when working with clients that
-    # keep the connection open after upload or download (e.g browsers).
-    timeout = 60
+    # Number of second to wait for recv() or send() on unauthorized
+    # connections.  When the timeout expires we close the connection.
+    # Authorized connections get a larger timeout using the ticket
+    # inactivity timeout.
+    timeout = 15
 
     # For generating connection ids. Start from 1 to match the connection
     # thread name.
@@ -270,6 +271,10 @@ class Connection(http.server.BaseHTTPRequestHandler):
         Used in Server header.
         """
         return "imageio/" + version.string
+
+    def set_timeout(self, timeout):
+        log.debug("Setting connection timeout to %s seconds", timeout)
+        self.connection.settimeout(timeout)
 
 
 class Request:
@@ -452,6 +457,9 @@ class Request:
         Return True if the underlying socket was disconnected.
         """
         return self._con.connection_error() in _DISCONNECTED
+
+    def set_connection_timeout(self, timeout):
+        self._con.set_timeout(timeout)
 
 
 class Response:

test/admin_test.py
@@ -89,6 +89,7 @@ def test_get_ticket(srv, fake_time):
             "size": ticket["size"],
             "sparse": ticket["sparse"],
             "dirty": ticket["dirty"],
+            "inactivity_timeout": ticket["inactivity_timeout"],
             "timeout": ticket["timeout"],
             "url": ticket["url"],
             "uuid": ticket["uuid"],

test/auth_test.py
@@ -50,13 +50,18 @@ class Operation:
         self.canceled = True
 
 
-def test_transfered_nothing():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+@pytest.fixture
+def cfg():
+    return config.load(["test/conf.d/daemon.conf"])
+
+
+def test_transfered_nothing(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     assert ticket.transferred() == 0
 
 
-def test_transfered_inactive_empty_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_inactive_empty_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.run(Operation(0, 0))
     assert ticket.transferred() == 0
 
@@ -64,8 +69,8 @@ def test_transfered_inactive_empty_ops():
     assert ticket.transferred() == 0
 
 
-def test_transfered_inactive_ordered_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_inactive_ordered_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.run(Operation(0, 100))
     assert ticket.transferred() == 100
 
@@ -76,8 +81,8 @@ def test_transfered_inactive_ordered_ops():
     assert ticket.transferred() == 300
 
 
-def test_transfered_inactive_unordered_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_inactive_unordered_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.run(Operation(100, 100))
     assert ticket.transferred() == 100
 
@@ -88,8 +93,8 @@ def test_transfered_inactive_unordered_ops():
     assert ticket.transferred() == 300
 
 
-def test_transfered_inactive_overlapping_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_inactive_overlapping_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.run(Operation(0, 120))
     assert ticket.transferred() == 120
 
@@ -100,8 +105,8 @@ def test_transfered_inactive_overlapping_ops():
     assert ticket.transferred() == 300
 
 
-def test_transfered_inactive_non_continuous_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_inactive_non_continuous_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     # Run 2 non-continutes operations
     ticket.run(Operation(0, 100))
     ticket.run(Operation(200, 100))
@@ -112,8 +117,8 @@ def test_transfered_inactive_non_continuous_ops():
     assert ticket.transferred() == 300
 
 
-def test_transfered_ongoing_concurrent_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_ongoing_concurrent_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Start 2 ongoing operations:
     # ongoing: 0-0, 100-100
@@ -145,8 +150,8 @@ def test_transfered_ongoing_concurrent_ops():
     assert not ticket.active()
 
 
-def test_transfered_ongoing_overlapping_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_ongoing_overlapping_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Start 2 ongoing operations.
     # ongoing: 0-0, 80-80
@@ -175,8 +180,8 @@ def test_transfered_ongoing_overlapping_ops():
     assert not ticket.active()
 
 
-def test_transfered_ongoing_non_continues_ops():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_transfered_ongoing_non_continues_ops(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Start 2 ongoing operations.
     # ongoing: 0-0, 200-200
@@ -204,9 +209,9 @@ def test_transfered_ongoing_non_continues_ops():
 
 
 @pytest.mark.benchmark
-def test_run_operation_benchmark():
+def test_run_operation_benchmark(cfg):
     # Run 1000000 operations with 4 concurrent threads.
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     operations = 10**6
     workers = 4
     chunk = 10**9
@@ -236,9 +241,9 @@ def test_run_operation_benchmark():
 
 @pytest.mark.benchmark
 @pytest.mark.parametrize("concurrent", [1, 2, 4, 8])
-def test_transferred_benchmark(concurrent):
+def test_transferred_benchmark(concurrent, cfg):
     # Time trransferred call with multiple ongoing and completed operations.
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     calls = 10000
 
@@ -271,9 +276,9 @@ def test_transferred_benchmark(concurrent):
     False,
     None
 ])
-def test_invalid_argument(arg):
+def test_invalid_argument(arg, cfg):
     with pytest.raises(errors.InvalidTicket):
-        Ticket(arg)
+        Ticket(arg, cfg)
 
 
 @pytest.mark.parametrize("kw", [
@@ -286,45 +291,53 @@ def test_invalid_argument(arg):
     {"filename": 1},
     {"sparse": 1},
     {"dirty": 1},
+    {"inactivity_timeout": "invalid"},
 ])
-def test_invalid_parameter(kw):
+def test_invalid_parameter(kw, cfg):
     with pytest.raises(errors.InvalidTicketParameter):
-        Ticket(testutil.create_ticket(**kw))
+        Ticket(testutil.create_ticket(**kw), cfg)
 
 
-def test_sparse_unset():
-    ticket = Ticket(testutil.create_ticket())
+def test_inactivity_timeout_unset(cfg):
+    ticket = Ticket(testutil.create_ticket(inactivity_timeout=None), cfg)
+    assert ticket.inactivity_timeout == cfg.daemon.inactivity_timeout
+
+
+def test_sparse_unset(cfg):
+    ticket = Ticket(testutil.create_ticket(), cfg)
     assert not ticket.sparse
 
 
-def test_sparse():
-    ticket = Ticket(testutil.create_ticket(sparse=True))
+def test_sparse(cfg):
+    ticket = Ticket(testutil.create_ticket(sparse=True), cfg)
     assert ticket.sparse
 
 
-def test_dirty_unset():
-    ticket = Ticket(testutil.create_ticket())
+def test_dirty_unset(cfg):
+    ticket = Ticket(testutil.create_ticket(), cfg)
     assert not ticket.dirty
 
 
-def test_dirty():
-    ticket = Ticket(testutil.create_ticket(dirty=True))
+def test_dirty(cfg):
+    ticket = Ticket(testutil.create_ticket(dirty=True), cfg)
     assert ticket.dirty
 
 
-def test_transfer_id_unset():
-    ticket = Ticket(testutil.create_ticket())
+def test_transfer_id_unset(cfg):
+    ticket = Ticket(testutil.create_ticket(), cfg)
     assert ticket.transfer_id is None
 
 
-def test_transfer_id():
-    ticket = Ticket(testutil.create_ticket(transfer_id="123"))
+def test_transfer_id(cfg):
+    ticket = Ticket(testutil.create_ticket(transfer_id="123"), cfg)
     assert ticket.transfer_id == "123"
 
 
-def test_repr():
-    ticket = Ticket(testutil.create_ticket(
-        ops=["read"], filename="tmp_file"))
+def test_repr(cfg):
+    ticket = Ticket(
+        testutil.create_ticket(
+            ops=["read"], filename="tmp_file"),
+        cfg)
     ticket_repr = repr(ticket)
 
     info = ticket.info()
@@ -335,8 +348,8 @@ def test_repr():
         assert pair in ticket_repr
 
 
-def test_ticket_run():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_ticket_run(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     op = Operation(0, 100)
     assert ticket.transferred() == op.done
     assert op.done == 0
@@ -347,8 +360,8 @@ def test_ticket_run():
     assert op.done == 100
 
 
-def test_cancel_no_connection():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_no_connection(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.cancel()
 
     # Ticket is canceled and can be removed immediately.
@@ -359,8 +372,8 @@ def test_cancel_no_connection():
     assert info["connections"] == 0
 
 
-def test_cancel_idle_connection():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_idle_connection(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ctx = Context()
     ticket.add_context(1, ctx)
     ticket.cancel()
@@ -378,8 +391,8 @@ def test_cancel_idle_connection():
     assert info["connections"] == 1
 
 
-def test_cancel_timeout():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_timeout(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Add conection - having connections does not block cancelation, but we
     # cannot have ongoing operations without a connection.
@@ -405,8 +418,8 @@ def test_cancel_timeout():
     assert info["connections"] == 1
 
 
-def test_cancel_async():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_async(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ctx = Context()
     ticket.add_context(1, ctx)
     ticket._add_operation(Operation(0, 100))
@@ -422,8 +435,8 @@ def test_cancel_async():
     assert info["connections"] == 1
 
 
-def test_cancel_ongoing_operations():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_ongoing_operations(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Few connections are using this ticket. Each running an operation.
     ops = []
@@ -447,8 +460,8 @@ def test_cancel_ongoing_operations():
     assert not idle_ctx.closed
 
 
-def test_cancel_wait():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_cancel_wait(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     # Add connections using this ticket.
     users = []
@@ -496,8 +509,8 @@ def test_cancel_wait():
     assert idle_ctx.closed
 
 
-def test_canceled_fail_run_before():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_canceled_fail_run_before(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.cancel()
 
     op = Operation()
@@ -510,8 +523,8 @@ def test_canceled_fail_run_before():
     assert op.done == 0
 
 
-def test_canceled_fail_run_after():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_canceled_fail_run_after(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
 
     class Operation:
 
@@ -536,8 +549,8 @@ def test_canceled_fail_run_after():
     assert op.done
 
 
-def test_canceled_fail_add_context():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_canceled_fail_add_context(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.cancel()
 
     ctx = Context()
@@ -547,21 +560,21 @@ def test_canceled_fail_add_context():
         ticket.add_context(2, ctx)
 
 
-def test_get_context_missing():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_get_context_missing(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     with pytest.raises(KeyError):
         ticket.get_context(1)
 
 
-def test_get_context():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_get_context(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ctx = Context()
     ticket.add_context(1, ctx)
     assert ticket.get_context(1) is ctx
 
 
-def test_remove_context_missing():
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+def test_remove_context_missing(cfg):
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ticket.add_context(1, Context())
     assert ticket.info()["connections"] == 1
 
@@ -569,7 +582,7 @@ def test_remove_context_missing():
     assert ticket.info()["connections"] == 1
 
 
-def test_remove_context_error():
+def test_remove_context_error(cfg):
 
     class FailingContext:
 
@@ -583,7 +596,7 @@ def test_remove_context_error():
                 raise RuntimeError("Cannot close yet")
             self.closed = True
 
-    ticket = Ticket(testutil.create_ticket(ops=["read"]))
+    ticket = Ticket(testutil.create_ticket(ops=["read"]), cfg)
     ctx = FailingContext()
     ticket.add_context(1, ctx)
 
@@ -601,8 +614,7 @@ def test_remove_context_error():
     assert ctx.closed
 
 
-def test_authorizer_add():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_add(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["read"])
     auth.add(ticket_info)
@@ -611,8 +623,7 @@ def test_authorizer_add():
     assert ticket.uuid == ticket_info["uuid"]
 
 
-def test_authorizer_remove_unused():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_remove_unused(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["read"])
     auth.add(ticket_info)
@@ -623,8 +634,7 @@ def test_authorizer_remove_unused():
         auth.get(ticket_info["uuid"])
 
 
-def test_authorizer_remove_timeout():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_remove_timeout(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["read"])
     auth.add(ticket_info)
@@ -690,8 +700,7 @@ def test_authorizer_remove_timeout():
         auth.get(ticket.uuid)
 
 
-def test_authorizer_remove_async():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_remove_async(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["read"])
     auth.add(ticket_info)
@@ -750,15 +759,13 @@ def test_authorizer_remove_async():
         auth.get(ticket.uuid)
 
 
-def test_authorizer_remove_mising():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_remove_mising(cfg):
     auth = Authorizer(cfg)
     # Removing missing ticket does not raise.
     auth.remove("no-such-ticket")
 
 
-def test_authorize_read():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorize_read(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["read"])
     auth.add(ticket_info)
@@ -770,8 +777,7 @@ def test_authorize_read():
         auth.authorize(ticket.uuid, "write")
 
 
-def test_authorize_write():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorize_write(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["write"])
     auth.add(ticket_info)
@@ -783,8 +789,7 @@ def test_authorize_write():
     assert auth.authorize(ticket.uuid, "read") == ticket
 
 
-def test_authorizer_no_ticket():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_no_ticket(cfg):
     auth = Authorizer(cfg)
     with pytest.raises(errors.AuthorizationError):
         auth.authorize("no-such-ticket", "read")
@@ -794,8 +799,7 @@ def test_authorizer_no_ticket():
     (["read"], ["read"]),
     (["write"], ["read", "write"]),
 ])
-def test_authorizer_canceled(ops, allowed):
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_canceled(ops, allowed, cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=ops)
     auth.add(ticket_info)
@@ -809,8 +813,7 @@ def test_authorizer_canceled(ops, allowed):
             auth.authorize(ticket.uuid, op)
 
 
-def test_authorizer_expired():
-    cfg = config.load(["test/conf.d/daemon.conf"])
+def test_authorizer_expired(cfg):
     auth = Authorizer(cfg)
     ticket_info = testutil.create_ticket(ops=["write"])
     auth.add(ticket_info)

test/backends_test.py
@@ -25,6 +25,10 @@ class Request:
 
     def __init__(self):
         self.context = {}
+        self.connection_timeout = None
+
+    def set_connection_timeout(self, timeout):
+        self.connection_timeout = timeout
 
 
 @pytest.fixture
@@ -34,7 +38,7 @@ def cfg():
 
 def test_get_caching(tmpurl, cfg):
     ticket = auth.Ticket(
-        testutil.create_ticket(url=urlunparse(tmpurl)))
+        testutil.create_ticket(url=urlunparse(tmpurl)), cfg)
     req = Request()
     c1 = backends.get(req, ticket, cfg)
 
@@ -55,9 +59,24 @@ def test_get_caching(tmpurl, cfg):
     assert c3 is not c1
 
 
+def test_get_set_timeout(tmpurl, cfg):
+    ticket = auth.Ticket(
+        testutil.create_ticket(
+            url=urlunparse(tmpurl),
+            inactivity_timeout=300),
+        cfg)
+
+    req = Request()
+    assert req.connection_timeout is None
+
+    # Authorizing the ticket set connection timeout.
+    backends.get(req, ticket, cfg)
+    assert req.connection_timeout == ticket.inactivity_timeout
+
+
 def test_get_canceled_ticket(tmpurl, cfg):
     ticket = auth.Ticket(
-        testutil.create_ticket(url=urlunparse(tmpurl)))
+        testutil.create_ticket(url=urlunparse(tmpurl)), cfg)
     req = Request()
     ticket.cancel()
 
@@ -76,7 +95,7 @@ def test_get_canceled_ticket(tmpurl, cfg):
 ])
 def test_get_ops(tmpurl, cfg, ops, readable, writable):
     ticket = auth.Ticket(
-        testutil.create_ticket(url=urlunparse(tmpurl), ops=ops))
+        testutil.create_ticket(url=urlunparse(tmpurl), ops=ops), cfg)
     req = Request()
     b = backends.get(req, ticket, cfg).backend
 
@@ -89,7 +108,7 @@ def test_get_ops(tmpurl, cfg, ops, readable, writable):
 @pytest.mark.parametrize("sparse", [True, False])
 def test_get_sparse(tmpurl, cfg, sparse):
     ticket = auth.Ticket(
-        testutil.create_ticket(url=urlunparse(tmpurl), sparse=sparse))
+        testutil.create_ticket(url=urlunparse(tmpurl), sparse=sparse), cfg)
     req = Request()
     b = backends.get(req, ticket, cfg).backend
 
@@ -110,7 +129,7 @@ def test_get_nbd_backend(tmpdir, cfg, nbd_server, transport):
     nbd_server.start()
 
     ticket = auth.Ticket(
-        testutil.create_ticket(url=urlunparse(nbd_server.url)))
+        testutil.create_ticket(url=urlunparse(nbd_server.url)), cfg)
     req = Request()
     b = backends.get(req, ticket, cfg).backend
 

commit 7a3541adcb1cc19065e499a173ef28056125d0da

config: Remove unused daemon:auth_timeout
When we added configurable inactivity timeout, we added also auth
timeout but it was never used. The value was hardcoded to 15 seconds in
2.4.3 and was reverted back to 60 seconds in 2.4.5.

Fixes: d5e9c757e0a4 (http: Configurable inactivity timeout)
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

data/README
@@ -47,11 +47,6 @@
 # The default value:
 #   max_connections = 8
 
-# Number of seconds before unauthorized connections are
-# disconnected.
-# The default value:
-#   auth_timeout = 15
-
 # Number of seconds before inactive connections are disconnected.
 # Clients with special needs can request a larger timeout when
 # creating an image transfer.

ovirt_imageio/_internal/config.py
@@ -20,10 +20,6 @@ class daemon:
     # decrease throughput.
     max_connections = 8
 
-    # Number of seconds before unauthorized connections are
-    # disconnected.
-    auth_timeout = 15
-
     # Number of seconds before inactive connections are disconnected.
     # Clients with special needs can request a larger timeout when
     # creating an image transfer.