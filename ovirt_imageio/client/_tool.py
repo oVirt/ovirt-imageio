65502c649bd183420ac9ae6fa7159daf8a8a4c7a
Nir Soffer <nsoffer@redhat.com>
2022-07-28 17:34:09 +0300

    ovirt-img: Terminate gracefully on signals
    Register signals handlers before running the command, and handle
    TerminatedBySignal exception gracefully.
    
    When terminated by signal, log an error instead of a traceback, since
    this is expected condition. Since termination by SIGINT is typically
    initiated by a user log info level message.
    
    For any other error, log a traceback to make it easy to report useful
    bug reports.
    
    An example run when ovirt-img is interrupted:
    
        $ ./ovirt-img download-disk -c engine adb9b2c7 dl.qcow2
        [  49% ] 3.00 GiB, 4.68 s, 656.51 MiB/s | command failed
    
    An example run when ovirt-img is interrupted with more verbose logging:
    
        $ ./ovirt-img download-disk -c engine adb9b2c7 dl.qcow2 --log-level info
        ...
        [  70% ] 4.25 GiB, 3.62 s, 1.17 GiB/s | command failed
        2022-07-28 18:08:16,562 INFO    (MainThread) [tool] Exiting: Terminated by signal 2
    
    An example run when ovirt-img is terminated::
    
        $ ./ovirt-img download-disk -c engine 07b53997 dl.qcow2
        [  26% ] 13.25 GiB, 26.23 s, 517.27 MiB/s | command failed
        2022-07-28 18:15:53,518 ERROR   (MainThread) [tool] Exiting: Terminated by signal 15
    
    An example run when qemu-nbd is terminated:
    
        $ ./ovirt-img download-disk -c engine 07b53997 dl.qcow2
        [  17% ] 8.75 GiB, 16.74 s, 535.39 MiB/s | command failed
        2022-07-28 18:20:23,721 ERROR   (MainThread) [tool] Command failed
        Traceback (most recent call last):
          File "ovirt-imageio/ovirt_imageio/client/_tool.py", line 38, in main
            args.command(args)
          File "ovirt-imageio/ovirt_imageio/client/_download.py", line 73, in download_disk
            _api.download(
          File "ovirt-imageio/ovirt_imageio/client/_api.py", line 179, in download
            _io.copy(
          File "ovirt-imageio/ovirt_imageio/client/_io.py", line 46, in copy
            with Executor(name=name) as executor:
          File "ovirt-imageio/ovirt_imageio/client/_io.py", line 198, in __exit__
            self.stop()
          File "ovirt-imageio/ovirt_imageio/client/_io.py", line 175, in stop
            raise self._errors[0]
          File "ovirt-imageio/ovirt_imageio/client/_io.py", line 248, in _run
            handler.copy(req)
          File "ovirt-imageio/ovirt_imageio/client/_io.py", line 292, in copy
            self._src.write_to(self._dst, req.length, self._buf)
          File "ovirt-imageio/ovirt_imageio/_internal/backends/http.py", line 220, in write_to
            writer.write(view[:n])
          File "ovirt-imageio/ovirt_imageio/_internal/backends/nbd.py", line 119, in write
            self._client.write(self._position, buf)
          File "ovirt-imageio/ovirt_imageio/_internal/nbd.py", line 445, in write
            self._recv_reply(cmd)
          File "ovirt-imageio/ovirt_imageio/_internal/nbd.py", line 959, in _recv_reply
            magic = self._recv_fmt("!I")[0]
          File "ovirt-imageio/ovirt_imageio/_internal/nbd.py", line 1221, in _recv_fmt
            data = self._recv(s.size)
          File "ovirt-imageio/ovirt_imageio/_internal/nbd.py", line 1231, in _recv
            self._recv_into(buf)
          File "ovirt-imageio/ovirt_imageio/_internal/nbd.py", line 1241, in _recv_into
            raise ProtocolError(
        ovirt_imageio._internal.nbd.ProtocolError: Server closed the connection, read 0 bytes, expected 4 bytes
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- ovirt_imageio/client/_tool.py
+++ ovirt_imageio/client/_tool.py
@@ -11,9 +11,14 @@ Tool for transferring disk images.
 """
 
 import logging
+import signal
+import sys
 
-from . import _options
+from . import _app
 from . import _download
+from . import _options
+
+log = logging.getLogger("tool")
 
 
 def main():
@@ -27,7 +32,15 @@ def main():
         format="%(asctime)s %(levelname)-7s (%(threadName)s) [%(name)s] "
                "%(message)s")
 
-    # XXX Setup signal handlers.
-    # XXX Handle commands errors.
+    _app.setup_signals()
 
-    args.command(args)
+    try:
+        args.command(args)
+    except _app.TerminatedBySignal as e:
+        # SIGINT is likey result of Control+C in the shell.
+        level = logging.INFO if e.signal == signal.SIGINT else logging.ERROR
+        log.log(level, "Exiting: %s", e)
+        sys.exit(128 + e.signal)
+    except Exception:
+        log.exception("Command failed")
+        sys.exit(1)
