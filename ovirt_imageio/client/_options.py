oVirt/ovirt-imageio

commit a2bbe122cc85d08dd5ab60e6d2438b54b1f11e6b

ovirt-img: Add logging options
The ovirt-img tool support now optional logging options:

  --log-file LOG_FILE   Log file name (default: /dev/stderr).
  --log-level LOG_LEVEL
                        Log level (choices: {debug, info, warning, error},
                        default: warning).

Users can change the default log file and level using the configuration
file:

    [engine]
    log_file = /home/user/.ovirt-img/engine.log
    log_level = info

Like other options, command lines options override values from the
config file.

Signed-off-by: Nir Soffer <nsoffer@redhat.com>

ovirt_imageio/client/_options.py
@@ -20,6 +20,29 @@ import os
 import sys
 
 
+class Choices:
+
+    def __init__(self, name, values):
+        self.name = name
+        self.values = values
+
+    def __call__(self, s):
+        if s not in self.values:
+            raise ValueError(
+                f"Invalid '{self.name}' value: '{s}', choices: {self}")
+        return s
+
+    def __str__(self):
+        s = ", ".join(self.values)
+        return f"{{{s}}}"
+
+    def __repr__(self):
+        return repr(self.name)
+
+
+log_level = Choices("log_level", ("debug", "info", "warning", "error"))
+
+
 class Option(
         namedtuple("Option", "name,args,config,required,type,default,help")):
 
@@ -88,6 +111,21 @@ class Parser:
             config=True,
             help=("Path to ovirt-engine CA certificate. If not set, read "
                   "from the specified config section"),
+        ),
+        Option(
+            name="log_file",
+            args=["--log-file"],
+            config=True,
+            default="/dev/stderr",
+            help="Log file name (default: /dev/stderr).",
+        ),
+        Option(
+            name="log_level",
+            args=["--log-level"],
+            config=True,
+            type=log_level,
+            default="warning",
+            help=(f"Log level (choices: {log_level}, default: warning)."),
         ),
     ]
 

ovirt_imageio/client/_ovirt.py
@@ -54,7 +54,9 @@ def connect(args):
         url=f"{args.engine_url}/ovirt-engine/api",
         username=args.username,
         password=password,
-        ca_file=args.cafile)
+        ca_file=args.cafile,
+        log=log if args.log_file else None,
+        debug=args.log_level == "debug")
 
 
 def find_disk(con, disk_id):

test/client_options_test.py
@@ -105,6 +105,8 @@ engine_url = https://engine.com
 username = username
 password = password
 cafile = /engine.pem
+log_file = /var/log/ovirt-img/engine.log
+log_level = info
 
 [required]
 engine_url = https://engine.com
@@ -121,6 +123,11 @@ username = username
 [missing3]
 #engine_url is not set
 #username is not set
+
+[invalid]
+engine_url = https://engine.com
+username = username
+log_level = invalid log level
 """)
 
 
@@ -131,6 +138,8 @@ def test_config_all(config):
     assert args.engine_url == "https://engine.com"
     assert args.username == "username"
     assert args.cafile == "/engine.pem"
+    assert args.log_file == "/var/log/ovirt-img/engine.log"
+    assert args.log_level == "info"
 
     # Use password from config.
     assert args.password_file is None
@@ -147,10 +156,14 @@ def test_config_all_override(config):
         "--username", "username2",
         "--password-file", "/password_file",
         "--cafile", "/engine2.pem",
+        "--log-file", "test.log",
+        "--log-level", "debug",
     ])
     assert args.engine_url == "https://engine2.com"
     assert args.username == "username2"
     assert args.cafile == "/engine2.pem"
+    assert args.log_file == "test.log"
+    assert args.log_level == "debug"
 
     # --password-file overrides password from config.
     assert args.password_file == "/password_file"
@@ -164,6 +177,8 @@ def test_config_required(config):
     assert args.engine_url == "https://engine.com"
     assert args.username == "username"
     assert args.cafile is None
+    assert args.log_file == "/dev/stderr"
+    assert args.log_level == "warning"
 
     # No --password-file or config password: get password from stdin.
     assert args.password_file is None
@@ -178,10 +193,14 @@ def test_config_required_override(config):
         "-c", "required",
         "--password-file", "/password_file",
         "--cafile", "/engine2.pem",
+        "--log-file", "test.log",
+        "--log-level", "debug",
     ])
     assert args.engine_url == "https://engine.com"
     assert args.username == "username"
     assert args.cafile == "/engine2.pem"
+    assert args.log_file == "test.log"
+    assert args.log_level == "debug"
 
     # Read password from --password-file.
     assert args.password_file == "/password_file"
@@ -214,6 +233,15 @@ def test_config_missing_override(config):
     ])
     assert args.engine_url == "https://engine.com"
     assert args.username == "username"
+
+
+def test_config_invalid(config, capsys):
+    parser = _options.Parser()
+    parser.add_sub_command("test", "help", lambda x: None)
+    with pytest.raises(SystemExit):
+        parser.parse(["test", "-c", "invalid"])
+    captured = capsys.readouterr()
+    assert repr("invalid log level") in captured.err
 
 
 def test_config_no_section(config, capsys):

ovirt_imageio/client/_tool.py
@@ -10,6 +10,8 @@
 Tool for transferring disk images.
 """
 
+import logging
+
 from . import _options
 from . import _download
 
@@ -19,7 +21,12 @@ def main():
     _download.register(parser)
     args = parser.parse()
 
-    # XXX Configure logging.
+    logging.basicConfig(
+        filename=args.log_file,
+        level=args.log_level.upper(),
+        format="%(asctime)s %(levelname)-7s (%(threadName)s) [%(name)s] "
+               "%(message)s")
+
     # XXX Setup signal handlers.
     # XXX Handle commands errors.
 

commit 666bf826447a618f8c519e3024f2e59aabafbf66

ovirt-img: fix logging config
In ovit-img log_file defaults to /dev/stderr fd,
and is passed to logging configurator through
filename argument.

However, this is not correct, filename only is
used for files. File descriptions shall go
as stream parameters.

Otherwise the tool breaks with
'OSError: [Errno 29] Illegal seek'
in an internal logging module.
Reproduced with Python 3.6 in CentOS Stream 8.

To solve it, default log_file to None instead, so
that logging configuration does not use a filename
and falls back to the stream argument, which defaults
to stderr.

Fixes: a2bbe122cc85d08dd5ab60e6d2438b54b1f11e6b
Fixes: #113
Signed-off-by: Albert Esteve <aesteve@redhat.com>

ovirt_imageio/client/_options.py
@@ -117,8 +117,7 @@ class Parser:
             name="log_file",
             args=["--log-file"],
             config=True,
-            default="/dev/stderr",
-            help="Log file name (default: /dev/stderr).",
+            help="Log to file instead of stderr.",
         ),
         Option(
             name="log_level",

ovirt_imageio/client/_ovirt.py
@@ -46,7 +46,7 @@ def connect(args):
         username=args.username,
         password=args.password,
         ca_file=args.cafile,
-        log=log if args.log_file else None,
+        log=log,
         debug=args.log_level == "debug")
 
 

test/client_options_test.py
@@ -185,7 +185,7 @@ def test_config_required(config, monkeypatch):
     assert args.engine_url == "https://engine.com"
     assert args.username == "username"
     assert args.cafile is None
-    assert args.log_file == "/dev/stderr"
+    assert args.log_file is None
     assert args.log_level == "warning"
 
     # No --password-file or config password: use getpass.getpass().