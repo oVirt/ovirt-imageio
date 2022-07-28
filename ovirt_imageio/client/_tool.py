9a5eae9092bd3e5b8a4cda6e49a490bf56809c7d
Albert Esteve <102953499+aesteve-rh@users.noreply.github.com>
2022-07-28 17:13:00 +0200

    Revert "ovirt-img: Terminate gracefully on signals"
    This reverts commit e1c73466c91466f915b788c97f81f8e97d2ff619.

--- ovirt_imageio/client/_tool.py
+++ ovirt_imageio/client/_tool.py
@@ -11,16 +11,9 @@ Tool for transferring disk images.
 """
 
 import logging
-import os
-import signal
-import sys
 
 from . import _options
 from . import _download
-
-log = logging.getLogger("tool")
-
-terminated = None
 
 
 def main():
@@ -34,23 +27,7 @@ def main():
         format="%(asctime)s %(levelname)-7s (%(threadName)s) [%(name)s] "
                "%(message)s")
 
-    signal.signal(signal.SIGINT, _handle_signal)
-    signal.signal(signal.SIGTERM, _handle_signal)
+    # XXX Setup signal handlers.
+    # XXX Handle commands errors.
 
-    try:
-        args.command(args)
-    except Exception:
-        if terminated:
-            log.error("Terminated by signal %d", terminated)
-            sys.exit(128 + terminated)
-        else:
-            log.exception("Command failed")
-            sys.exit(1)
-
-
-def _handle_signal(signo, frame):
-    global terminated
-    if not terminated:
-        terminated = signo
-        # Terminate qemu-nbd triggering normal cleanup flow.
-        os.killpg(0, signo)
+    args.command(args)
