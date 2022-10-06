a5aa34ef510951cf78e0edfb2d7e11ba76ce04d7
Albert Esteve <aesteve@redhat.com>
2022-10-06 13:37:52 +0200

    reuse: addheader *.go
    Add SPDX header to all GOLang files
    in the project.
    
    Files in the go/imageio directory are
    licensed under LGPL, which needs to be
    added to the LICENSES folder.
    
    The rest can just use the GPL license
    used in the rest of the project.
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- go/imageio/http/http_test.go
+++ go/imageio/http/http_test.go
@@ -1,10 +1,5 @@
-// ovirt-imageio
-// Copyright (C) 2021 Red Hat, Inc.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
+// SPDX-FileCopyrightText: Red Hat, Inc.
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 package http
 
